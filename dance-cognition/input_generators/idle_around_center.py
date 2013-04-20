68c704bbd735af70105ad59dde7ce0503420132a
Alexander Berman <alex@nimations.com>
2013-04-20 11:55:40 +0200

    added pause to idle_around_center, making it more realistic

--- dance-cognition/input_generators/idle_around_center.py
+++ dance-cognition/input_generators/idle_around_center.py
@@ -4,28 +4,44 @@ import math
 import input_generator
 
 class Generator(input_generator.Generator):
+    center = Vector3d(0.0, 0.0, 0.0)
+
     @staticmethod
     def add_parser_arguments(parser):
         parser.add_argument("-magnitude", type=float, default=0.1)
+        parser.add_argument("-pause-duration", type=float, default=0.2)
+        parser.add_argument("-sway-duration", type=float, default=1.0)
 
     def __init__(self, args):
         self._magnitude = args.magnitude
         self._position = Vector3d(0, 0, 0)
         self._t = 0
-        self._sway_duration = 1.0
+        self._sway_duration = args.sway_duration
+        self._pause_duration = args.pause_duration
         self._sway_target = None
+        self._pausing = True
 
     def update(self, time_increment):
         self._t += time_increment
-        if self._t > self._sway_duration or self._sway_target is None:
-            self._sway_target = Vector3d(
-                random.uniform(-1, 1),
-                random.uniform(-1, 1),
-                random.uniform(-1, 1)) * self._magnitude
-            self._t = 0
+
+        if self._pausing:
+            if self._t > self._pause_duration:
+                self._pausing = False
+                self._t = 0
+                self._sway_target = Vector3d(
+                    random.uniform(-1, 1),
+                    random.uniform(-1, 1),
+                    random.uniform(-1, 1)) * self._magnitude
+        else:
+            if self._t > self._sway_duration:
+                self._pausing = True
+                self._t = 0
 
     def position(self):
-        return self._sway_target * self._envelope(self._t / self._sway_duration)
+        if self._pausing:
+            return self.center
+        else:
+            return self._sway_target * self._envelope(self._t / self._sway_duration)
 
     def _envelope(self, x):
         if x < 0.5:
