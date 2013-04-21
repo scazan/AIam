473d29acbdfe4882850604f5245fcf49d9ec075b
Alexander Berman <alex@nimations.com>
2013-04-21 09:46:27 +0200

    improved sensory adaptation

--- dance-cognition/sensory_adaptation.py
+++ dance-cognition/sensory_adaptation.py
@@ -1,38 +1,11 @@
 from vector import *
-import math
-
-# response_factor = 1.0
-# activity_magnitude_threshold = 0.01
-# passivity_duration_threshold = 5.0
-
-# class SensoryAdapter:
-#     def __init__(self):
-#         self._background = Vector2d(0.0, 0.0)
-#         self._previous_stimulus = Vector2d(0.0, 0.0)
-#         self._passivity_duration = 0.0
-
-#     def feed_stimulus(self, stimulus, time_increment):
-#         if (self._previous_stimulus - stimulus).mag() > activity_magnitude_threshold:
-#             self._passivity_duration = 0.0
-#         else:
-#             self._passivity_duration += time_increment
-
-#         if self._passivity_duration > passivity_duration_threshold:
-#             self._background += (stimulus - self._background) * \
-#                 min(response_factor * time_increment, 1.0)
-
-#         self._previous_stimulus.set(stimulus)
-
-#     def response(self):
-#         return self._previous_stimulus - self._background
-
-#     def background(self):
-#         return self._background
 
 class SensoryAdapter:
     def __init__(self):
-        self._background = Vector3d(0.0, 0.0, 0.0)
+        self._sensed_center = Vector3d(0.0, 0.0, 0.0)
 
-    def process(self, signal, time_increment):
-        self._background += (signal - self._background) * 0.2 * min(time_increment, 1.0)
-        return signal - self._background
+    def process(self, position, time_increment):
+        output = position - self._sensed_center
+        factor = 0.1 * time_increment / max(output.mag(), 0.0001)
+        self._sensed_center += output * min(factor, 1.0)
+        return output
