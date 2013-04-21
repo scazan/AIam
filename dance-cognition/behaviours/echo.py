6ea33c5015ec454baf3c76a1d4b6e16ca383e558
Alexander Berman <alex@nimations.com>
2013-04-21 16:40:13 +0200

    tranistions in echo copy the duration of transitions in the input

--- dance-cognition/behaviours/echo.py
+++ dance-cognition/behaviours/echo.py
@@ -12,8 +12,10 @@ class Behaviour(behaviour.Behaviour):
         self._observed_state = None
         self._output_queue = []
         self._output_transition = None
+        self._time = 0.0
 
     def process_input(self, input_position, time_increment):
+        self._time += time_increment
         self._input_position = input_position
         nearest_state = self._nearest_state()
         if nearest_state and self._distance_to_state(nearest_state) < SPATIAL_THRESHOLD:
@@ -27,12 +29,11 @@ class Behaviour(behaviour.Behaviour):
 
         if self._output_transition:
             self._output_transition_time += time_increment
-            if self._output_transition_time > self._output_transition_duration:
+            if self._output_transition_time > self._output_transition["duration"]:
                 self._output_transition = None
 
         if not self._output_transition and len(self._output_queue) > 0:
             self._output_transition = self._output_queue.pop(0)
-            self._output_transition_duration = 1.0 # TEMP
             self._output_transition_time = 0.0
 
     def _nearest_state(self):
@@ -43,21 +44,25 @@ class Behaviour(behaviour.Behaviour):
 
     def _state_observed(self, new_observed_state):
         if self._observed_state and self._is_valid_transition(self._observed_state, new_observed_state):
-            self._transition_observed(self._observed_state, new_observed_state)
+            duration = self._time - self._observed_state_at_time
+            self._transition_observed(self._observed_state, new_observed_state, duration)
         self._observed_state = new_observed_state
+        self._observed_state_at_time = self._time
 
     def _is_valid_transition(self, source_state, destination_state):
         return destination_state in source_state.inputs + source_state.outputs
 
-    def _transition_observed(self, source_state, destination_state):
-        self._output_queue.append((source_state, destination_state))
+    def _transition_observed(self, source_state, destination_state, duration):
+        self._output_queue.append({"source_state": source_state,
+                                   "destination_state": destination_state,
+                                   "duration": duration})
 
     def output(self):
         if self._output_transition:
-            source_state, destination_state = self._output_transition
             return InterStatePosition(
-                source_state, destination_state,
-                self._output_transition_time / self._output_transition_duration)
+                self._output_transition["source_state"],
+                self._output_transition["destination_state"],
+                self._output_transition_time / self._output_transition["duration"])
 
     def observed_state(self):
         return self._observed_state
