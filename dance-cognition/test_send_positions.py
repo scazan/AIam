9893f609bc5b32f36dc92c1f3dab660f52fd91ff
Alexander Berman <alex@nimations.com>
2013-04-26 14:52:49 +0200

    updated tasks list

--- dance-cognition/test_send_positions.py
+++ dance-cognition/test_send_positions.py
@@ -3,10 +3,9 @@ from simple_osc_sender import OscSender
 osc_sender = OscSender(7892)
 
 def move(source, destination):
-    for i in range(10):
-        osc_sender.send("/position", source, destination, float(i) / 10)
-        print "/position", source, destination, float(i) / 10
-        time.sleep(0.1)
+    for i in range(100):
+        osc_sender.send("/position", source, destination, float(i) / 100)
+        time.sleep(0.01)
 
 while True:
     move("mc", "ml")
