1e1fba58e5295e47fb1b0d8128f4569f9fa3f67a
Alex Berman <alex@nimations.com>
2013-04-26 11:54:47 +0200

    test for receiving position over OSC now works

--- dance-cognition/test_receive_position.py
+++ dance-cognition/test_receive_position.py
@@ -5,3 +5,5 @@ def receive_position(path, args, types, src, user_data):
     
 server =  liblo.Server(7892, liblo.UDP)
 server.add_method("/position", "ssf", receive_position)
+while True:
+    server.recv(100)
