897fb4f539bc24423d86cab3c2d2e6ae16180d32
Alexander Berman <alex@nimations.com>
2013-04-28 08:04:23 +0200

    refactoring

--- dance-cognition/behaviour.py
+++ dance-cognition/behaviour.py
@@ -1,7 +1,3 @@
-# TODO:
-# add method leave_free_movement. this will be used by echo_or_mirror when detecting that partner is away from center.
-# add callback for entering resting state (from free movement). this will be used by echo_or_mirror to potentially start new move.
-
 from states import state_machine, MC
 
 class Behaviour:
@@ -10,6 +6,25 @@ class Behaviour:
         self.motion_controller = motion_controller
         self.MC = state_machine.states[MC]
         self.enabled = True
+        self._mode = None
+        self._sub_behaviours = {}
+        self._sub_behaviour = None
 
     def process_input(self, input_position, time_increment):
         self._time_increment = time_increment
+        if self._sub_behaviour:
+            self._sub_behaviour.process_input(input_position, time_increment)
+
+    def add_mode(self, module, *args):
+        self._sub_behaviours[module] = module.Behaviour(*args)
+
+    def get_mode(self):
+        return self._mode
+
+    def set_mode(self, module):
+        if self._mode != module:
+            print "-> %s" % module.__name__
+            self._mode = module
+            self._sub_behaviour = self._sub_behaviours[module]
+            for sub_behaviour in self._sub_behaviours.values():
+                sub_behaviour.enabled = (sub_behaviour == self._sub_behaviour)
--- dance-cognition/behaviours/echo_or_mirror.py
+++ dance-cognition/behaviours/echo_or_mirror.py
@@ -1,44 +1,30 @@
 import behaviour
-import behaviours.mirror
-import behaviours.echo
+from behaviours import mirror
+from behaviours import echo
 import interpret
 
-MIRROR_THRESHOLD = 3.0
+PASSIVITY_TIMEOUT = 3.0
 
 class Behaviour(behaviour.Behaviour):
     def __init__(self, *args):
         behaviour.Behaviour.__init__(self, *args)
-        self._modes = {
-            "mirror": behaviours.mirror.Behaviour(*args),
-            "echo": behaviours.echo.Behaviour(*args),
-        }
-        self._mode_name = None
+        self.add_mode(mirror, *args)
+        self.add_mode(echo, *args)
         self._enable_mirror()
         self.interpreter.add_callback(interpret.STATE, self._observed_state)
         self.interpreter.add_passivity_callback(
-            MIRROR_THRESHOLD, self._enable_mirror)
-
-    def process_input(self, input_position, time_increment):
-        self._mode.process_input(input_position, time_increment)
+            PASSIVITY_TIMEOUT, self._enable_mirror)
 
     def _enable_mirror(self):
-        if self._mode_name != "mirror":
-            self._set_mode("mirror")
+        if self.get_mode() != mirror:
+            self.set_mode(mirror)
             self.motion_controller.initiate_idle()
 
     def _observed_state(self, state):
-        if self._mode != "echo" and state != self.MC:
+        if self.get_mode() != echo and state != self.MC:
             self._enable_echo()
 
     def _enable_echo(self):
-        if self._mode_name != "echo":
-            self._set_mode("echo")
+        if self.get_mode != echo:
+            self.set_mode(echo)
             self.motion_controller.initiate_idle() # go back to center before echoing
-
-    def _set_mode(self, mode_name):
-        if self._mode_name != mode_name:
-            print "-> %s" % mode_name
-            self._mode = self._modes[mode_name]
-            self._mode_name = mode_name
-            for mode in self._modes.values():
-                mode.enabled = (mode == self._mode)
