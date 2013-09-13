5ce4227bf815025713a5c6b2d1f503622aad0418
Alex Berman <alex@nimations.com>
2013-09-13 09:20:50 +0200

    towards a new experiment

---
+++ dance-cognition/experiments/feedforward.py
@@ -0,0 +1,64 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/..")
+
+from argparse import ArgumentParser
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import window
+from vector import Vector2d
+import math
+
+class InputGenerator:
+    def __init__(self):
+        self._t = 0
+
+    def process(self, time_increment):
+        self._t += time_increment
+        x = math.cos(self._t)
+        y = math.sin(self._t)
+        return Vector2d(x, y)
+
+class NeuralNet:
+    def process(self, inp):
+        return inp
+
+class Frame(window.Frame):
+    def draw_point(self, p):
+        glPointSize(3)
+        glBegin(GL_POINTS)
+        glVertex2f((p.x/2 + 0.5) * self.width,
+                   (p.y/2 + 0.5) * self.height)
+        glEnd()
+
+class InputFrame(Frame):
+    def render(self):
+        glColor3f(0, 0, 0)
+        self.draw_point(self.window.input)
+
+class OutputFrame(Frame):
+    def render(self):
+        glColor3f(0.5, 0.5, 1.0)
+        self.draw_point(self.window.output)
+
+class ExperimentWindow(window.Window):
+    def __init__(self, *args):
+        window.Window.__init__(self, *args)
+        self.input_generator = InputGenerator()
+        self.input = Vector2d(0,0)
+        self.output = Vector2d(0,0)
+        self.net = NeuralNet()
+        self.input_frame = InputFrame(
+            self, left=100, top=100, width=200, height=200)
+        self.output_frame = OutputFrame(
+            self, left=400, top=100, width=200, height=200)
+
+    def render(self):
+        self.input = self.input_generator.process(self.time_increment)
+        self.output = self.net.process(self.input)
+
+parser = ArgumentParser()
+window.Window.add_parser_arguments(parser)
+args = parser.parse_args()
+window.run(ExperimentWindow, args)
