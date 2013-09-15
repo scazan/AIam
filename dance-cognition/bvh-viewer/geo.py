98b9132515f431f3581d1978d6278d60e34936a4
Alex Berman <alex@nimations.com>
2013-09-15 15:14:47 +0200

    refactoring

--- dance-cognition/bvh-viewer/geo.py
+++ dance-cognition/bvh-viewer/geo.py
@@ -1,16 +1,11 @@
 # adopted from BVHplay (http://sourceforge.net/projects/bvhplay)
 #
 
-from numpy import array
-
-
 class vertex:
-    def __init__(self, x=0, y=0, z=0, description='', DEBUG=0):
-      self.tr = array([x,y,z,1])
+    def __init__(self, x=0, y=0, z=0):
+      self.tr = [x,y,z,1]
 
 class edge:
-    def __init__(self, v1, v2, description='', DEBUG=0):
+    def __init__(self, v1, v2):
       self.v1 = v1
       self.v2 = v2
-      self.descr = description
-      self.DEBUG = DEBUG
