6fa38eb37aef5c3d07c46d5b06a3ad7b857a8e8d
Alex Berman <alex@nimations.com>
2013-09-21 08:26:58 +0200

    feed-forward experiment now in 3d

--- dance-cognition/experiments/feedforward.py
+++ dance-cognition/experiments/feedforward.py
@@ -10,7 +10,7 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import window
-from vector import Vector2d
+from vector import Vector3d
 import math
 import collections
 from pybrain.tools.shortcuts import buildNetwork
@@ -23,45 +23,29 @@ class InputGenerator:
 
     def process(self, time_increment):
         self._t += time_increment
-        x = math.cos(self._t)
+        z = math.cos(self._t)
         y = math.sin(self._t)
-        return Vector2d(x, y)
+        x = 0
+        return Vector3d(x, y, z)
 
 class NeuralNet:
     def __init__(self):
-        self._net = buildNetwork(2, 3, 2)
+        self._net = buildNetwork(3, 6, 3)
         self._recent_data = collections.deque(maxlen=DATASET_SIZE)
         self._trainer = BackpropTrainer(self._net, learningrate=0.001)
 
     def process(self, inp):
         net_in = inp.v
         net_out = self._net.activate(net_in)
-        return Vector2d(*net_out)
+        return Vector3d(*net_out)
 
     def train(self, inp, output):
         self._recent_data.append((inp.v, output.v))
-        dataset = SupervisedDataSet(2, 2)
+        dataset = SupervisedDataSet(3, 3)
         for recent_in, recent_out in self._recent_data:
             dataset.addSample(recent_in, recent_out)
 
         self._trainer.trainOnDataset(dataset)
-
-class Frame(window.Frame):
-    def draw_point(self, p):
-        glPointSize(3)
-        glBegin(GL_POINTS)
-        glVertex2f((p.x/2 + 0.5) * self.width,
-                   (p.y/2 + 0.5) * self.height)
-        glEnd()
-
-class InputOutputFrame(Frame):
-    def render(self):
-        if self.window.input:
-            glColor3f(0, 1, 0)
-            self.draw_point(self.window.input)
-        if self.window.output:
-            glColor3f(0.5, 0.5, 1.0)
-            self.draw_point(self.window.output)
 
 class ExperimentWindow(window.Window):
     def __init__(self, *args):
@@ -72,10 +56,10 @@ class ExperimentWindow(window.Window):
         self.net = NeuralNet()
         self._input_history = collections.deque(maxlen=HISTORY_SIZE)
         self._input_history.extend([
-                Vector2d(0,0) for n in range(HISTORY_SIZE)])
+                Vector3d(0,0,0) for n in range(HISTORY_SIZE)])
         self._training_history = collections.deque(maxlen=HISTORY_SIZE)
-        self.input_and_output_frame = InputOutputFrame(
-            self, left=100, top=100, width=200, height=200)
+        self._y_orientation = 0.0
+        self._x_orientation = 0.0
 
     def render(self):
         self.input = self.input_generator.process(self.time_increment)
@@ -84,6 +68,34 @@ class ExperimentWindow(window.Window):
         self.output = self.net.process(self.input)
         self._training_history.append((self.past_input, self.input))
         self.net.train(*self._training_history[0])
+
+        self.configure_3d_projection(-100, 0)
+        glRotatef(self._x_orientation, 1.0, 0.0, 0.0)
+        glRotatef(self._y_orientation, 0.0, 1.0, 0.0)
+        self._draw_unit_cube()
+        self._draw_input()
+        self._draw_output()
+
+    def _draw_input(self):
+        if self.input:
+            glColor3f(0, 1, 0)
+            self._draw_point(self.input)
+
+    def _draw_output(self):
+        if self.output:
+            glColor3f(0.5, 0.5, 1.0)
+            self._draw_point(self.output)
+
+    def _draw_point(self, p):
+        glPointSize(3)
+        glBegin(GL_POINTS)
+        glVertex3f(p.x, p.y, p.z)
+        glEnd()
+
+    def _draw_unit_cube(self):
+        glColor4f(0,0,0,0.2)
+        glutWireCube(2.0)
+
 
 parser = ArgumentParser()
 window.Window.add_parser_arguments(parser)
