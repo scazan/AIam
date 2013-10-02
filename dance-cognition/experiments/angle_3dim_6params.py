44983d69db6bf001a917ff7f7c2f1878dc94ce1f
Alex Berman <alex@nimations.com>
2013-10-02 07:40:06 +0200

    refactoring

--- dance-cognition/experiments/angle_3dim_6params.py
+++ dance-cognition/experiments/angle_3dim_6params.py
@@ -1,17 +1,12 @@
 from experiment import *
-from angle_parameters import radians_to_vector2d, vector2d_to_radians
-import numpy
+from angle_parameters import radians3d_to_vector6d, vector6d_to_radians3d
 
 class SphericalStimulus(Stimulus):
     def get_value(self):
         r1 = (self._t / 1) % (2*math.pi)
         r2 = (self._t / 2) % (2*math.pi)
         r3 = (self._t / 4) % (2*math.pi)
-        return numpy.append(
-            radians_to_vector2d(r1),
-            numpy.append(
-                radians_to_vector2d(r2),
-                radians_to_vector2d(r3)))
+        return radians3d_to_vector6d(r1, r2, r3)
 
     def get_duration(self):
         return 2 * math.pi * 4
@@ -19,17 +14,11 @@ class SphericalStimulus(Stimulus):
 class AngleWindow(ExperimentWindow):
     def draw_input(self, inp):
         glColor3f(0, 1, 0)
-        self._draw_3dim_angle(*self._vector6d_to_3radians(inp))
+        self._draw_3dim_angle(*vector6d_to_radians3d(inp))
 
     def draw_output(self, output):
         glColor3f(0.5, 0.5, 1.0)
-        self._draw_3dim_angle(*self._vector6d_to_3radians(output))
-
-    def _vector6d_to_3radians(self, vector6d):
-        r1 = vector2d_to_radians(vector6d[0:2])
-        r2 = vector2d_to_radians(vector6d[2:4])
-        r3 = vector2d_to_radians(vector6d[4:6])
-        return r1, r2, r3
+        self._draw_3dim_angle(*vector6d_to_radians3d(output))
 
     def _draw_3dim_angle(self, r1, r2, r3):
         glPointSize(3)
--- dance-cognition/experiments/angle_parameters.py
+++ dance-cognition/experiments/angle_parameters.py
@@ -8,7 +8,19 @@ def radians_to_vector2d(radians):
 def vector2d_to_radians(v):
     dx, dy = v
     if dx == 0 and dy == 0:
-        print "RANDOM!"
         return random.uniform(0, 2*math.pi)
     else:
         return math.atan2(dy, dx)
+
+def radians3d_to_vector6d(r1, r2, r3):
+    return numpy.append(
+        radians_to_vector2d(r1),
+        numpy.append(
+            radians_to_vector2d(r2),
+            radians_to_vector2d(r3)))
+
+def vector6d_to_radians3d(vector6d):
+    r1 = vector2d_to_radians(vector6d[0:2])
+    r2 = vector2d_to_radians(vector6d[2:4])
+    r3 = vector2d_to_radians(vector6d[4:6])
+    return r1, r2, r3
