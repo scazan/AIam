scazan/AIam

commit 85b350a48dd738c559cbc2f48bb2d55a55c51da0

refactored and maybe optimized bvh_reader

dance-cognition/bvh_reader/bvh_reader.py
@@ -99,75 +99,26 @@ class skeleton:
 
     def _process_bvhkeyframe(self, keyframe, joint, t):
         counter = 0
-        transpose = False
-        rotate = False
-
-        rotation_matrix = array([
-            [1.,0.,0.,0.],
-            [0.,1.,0.,0.],
-            [0.,0.,1.,0.],
-            [0.,0.,0.,1.] ])
-
+        keyframe_dict = dict()
         for channel in joint.channels:
-            keyval = keyframe[counter]
-            if(channel == "Xposition"):
-                transpose = True
-                xpos = keyval
-            elif(channel == "Yposition"):
-                transpose = True
-                ypos = keyval
-            elif(channel == "Zposition"):
-                transpose = True
-                zpos = keyval
-
-            elif(channel == "Xrotation"):
-                rotate = True
-                xrot = keyval
-                theta = radians(xrot)
-                mycos = cos(theta)
-                mysin = sin(theta)
-                rotation_matrix2 = array([
-                    [1.,     0.,     0.,     0.],
-                    [0.,     mycos,  -mysin, 0.],
-                    [0.,     mysin,  mycos,  0.],
-                    [0.,     0.,     0.,     1.] ])
-                rotation_matrix = dot(rotation_matrix, rotation_matrix2)
-
-            elif(channel == "Yrotation"):
-                rotate = True
-                yrot = keyval
-                theta = radians(yrot)
-                mycos = cos(theta)
-                mysin = sin(theta)
-                rotation_matrix2 = array([
-                    [mycos,  0.,    mysin, 0.],
-                    [0.,     1.,    0.,    0.],
-                    [-mysin, 0.,    mycos, 0.],
-                    [0.,     0.,    0.,    1.] ])
-                rotation_matrix = dot(rotation_matrix, rotation_matrix2)
-
-            elif(channel == "Zrotation"):
-                rotate = True
-                zrot = keyval
-                theta = radians(zrot)
-                mycos = cos(theta)
-                mysin = sin(theta)
-                rotation_matrix2 = array([
-                    [mycos,  -mysin, 0.,   0.],
-                    [mysin,  mycos,  0.,   0.],
-                    [0.,     0.,     1.,   0.],
-                    [0.,     0.,     0.,   1.] ])
-                rotation_matrix = dot(rotation_matrix, rotation_matrix2)
-            else:
-                raise Exception("illegal channel name ", channel)
+            keyframe_dict[channel] = keyframe[counter]
             counter += 1
 
-        if transpose:
+        if "Xposition" in keyframe_dict:
             transposition_matrix = array([
-                [1.,    0.,    0.,    xpos],
-                [0.,    1.,    0.,    ypos],
-                [0.,    0.,    1.,    zpos],
+                [1.,    0.,    0.,    keyframe_dict["Xposition"]],
+                [0.,    1.,    0.,    keyframe_dict["Yposition"]],
+                [0.,    0.,    1.,    keyframe_dict["Zposition"]],
                 [0.,    0.,    0.,    1.] ])
+
+        if "Xrotation" in keyframe_dict:
+            rotate = True
+            rotation_matrix = self._rotation_matrix(
+                keyframe_dict["Xrotation"],
+                keyframe_dict["Yrotation"],
+                keyframe_dict["Zrotation"])
+        else:
+            rotate = False
 
         if joint.hasparent:
             parent_trtr = joint.parent.trtr
@@ -175,7 +126,10 @@ class skeleton:
         else:
             localtoworld = dot(joint.transposition_matrix, transposition_matrix)
 
-        trtr = dot(localtoworld, rotation_matrix)
+        if rotate:
+            trtr = dot(localtoworld, rotation_matrix)
+        else:
+            trtr = localtoworld
         joint.trtr = trtr
 
 
@@ -193,6 +147,45 @@ class skeleton:
                 raise Exception("fatal error")
 
         return newkeyframe
+
+    def _rotation_matrix(self, xrot, yrot, zrot):
+        rotation_matrix = array([
+            [1.,0.,0.,0.],
+            [0.,1.,0.,0.],
+            [0.,0.,1.,0.],
+            [0.,0.,0.,1.] ])
+
+        theta = radians(zrot)
+        mycos = cos(theta)
+        mysin = sin(theta)
+        rotation_matrix2 = array([
+            [mycos,  -mysin, 0.,   0.],
+            [mysin,  mycos,  0.,   0.],
+            [0.,     0.,     1.,   0.],
+            [0.,     0.,     0.,   1.] ])
+        rotation_matrix = dot(rotation_matrix, rotation_matrix2)
+
+        theta = radians(yrot)
+        mycos = cos(theta)
+        mysin = sin(theta)
+        rotation_matrix2 = array([
+            [mycos,  0.,    mysin, 0.],
+            [0.,     1.,    0.,    0.],
+            [-mysin, 0.,    mycos, 0.],
+            [0.,     0.,    0.,    1.] ])
+        rotation_matrix = dot(rotation_matrix, rotation_matrix2)
+
+        theta = radians(xrot)
+        mycos = cos(theta)
+        mysin = sin(theta)
+        rotation_matrix2 = array([
+            [1.,     0.,     0.,     0.],
+            [0.,     mycos,  -mysin, 0.],
+            [0.,     mysin,  mycos,  0.],
+            [0.,     0.,     0.,     1.] ])
+        rotation_matrix = dot(rotation_matrix, rotation_matrix2)
+
+        return rotation_matrix
 
 
 class BvhReader(cgkit.bvh.BVHReader):

commit 87ef9789c5806a971160c9c305e2887099c331d1

hopefully fixed remaining rotation problems which were introduced in 85b350a48dd738c559cbc2f48bb2d55a55c51da0. the previous fix (858cac50b965c1712b4ee8c4b3dbe60c7e27388c). the new solution reverts usage of x-y-z order specified in the BVH file, rather than a fixed order, and seems ok both for hb-hrf2.bvh and HDM_bk_03-01_02_120.bvh.

dance-cognition/bvh_reader/bvh_reader.py
@@ -87,10 +87,25 @@ class skeleton:
 
         if "Xrotation" in keyframe_dict:
             rotate = True
-            rotation_matrix = make_rotation_matrix(
-                keyframe_dict["Xrotation"],
-                keyframe_dict["Yrotation"],
-                keyframe_dict["Zrotation"])
+            rotation_matrix = array([
+                    [1.,0.,0.,0.],
+                    [0.,1.,0.,0.],
+                    [0.,0.,1.,0.],
+                    [0.,0.,0.,1.] ])
+            for channel in joint.channels:
+                if channel == "Xrotation":
+                    rotation_matrix = dot(
+                        rotation_matrix,
+                        make_x_rotation_matrix(keyframe_dict["Xrotation"]))
+                elif channel == "Yrotation":
+                    rotation_matrix = dot(
+                        rotation_matrix,
+                        make_y_rotation_matrix(keyframe_dict["Yrotation"]))
+                elif channel == "Zrotation":
+                    rotation_matrix = dot(
+                        rotation_matrix,
+                        make_z_rotation_matrix(keyframe_dict["Zrotation"]))
+
             joint.rotation = [keyframe_dict["Xrotation"],
                               keyframe_dict["Yrotation"],
                               keyframe_dict["Zrotation"]]

dance-cognition/bvh_reader/geo.py
@@ -17,11 +17,6 @@ def make_transposition_matrix(xpos, ypos, zpos):
             [0.,    0.,    1.,    zpos],
             [0.,    0.,    0.,    1.] ])
 
-def make_rotation_matrix(xrot, yrot, zrot):
-    return dot(dot(make_x_rotation_matrix(xrot),
-                   make_y_rotation_matrix(yrot)),
-               make_z_rotation_matrix(zrot))
-
 def make_z_rotation_matrix(degrees):
     theta = radians(degrees)
     mycos = cos(theta)