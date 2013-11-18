d8aecfd9c75271fcc9e1e3e7bd56c5c497ee7988
Alex Berman <alex@nimations.com>
2013-11-18 10:14:33 +0100

    Separate plot for each joint

--- dance-cognition/experiments/plot_joint_angles.py
+++ dance-cognition/experiments/plot_joint_angles.py
@@ -20,9 +20,9 @@ class JointAnglePlotter:
 
     def __init__(self, args):
         self.args = args
-        self.out = open(args.output, "w")
         self.bvh_reader = bvh_reader_module.BvhReader(args.bvh)
         self.bvh_reader.read()
+        self._create_outputs()
 
         self.dimension_plots = {}
         n = 0
@@ -32,42 +32,69 @@ class JointAnglePlotter:
                 "cy": self.args.spacing + self.args.radius}
             n += 1
 
+    def _create_outputs(self):
+        hips = self.bvh_reader.skeleton.get_hips(0)
+        self.joint_index = 0
+        self._identify_joints_with_rotation(hips)
+        self.num_joints_with_rotation = self.joint_index
+
+        self.outs = []
+        for n in range(self.num_joints_with_rotation):
+            output_path = "%s%d.svg" % (args.output, n)
+            self.outs.append(open(output_path, "w"))
+
+    def _identify_joints_with_rotation(self, joint):
+        if joint.rotation_definition:
+            joint.index_with_rotation = self.joint_index
+            self.joint_index += 1
+        for child in joint.children:
+            self._identify_joints_with_rotation(child)
+
     def plot(self):
-        self._add_header()
+        self._add_headers()
         for n in range(self.bvh_reader.skeleton.num_frames):
             hips = self.bvh_reader.skeleton.get_hips(n)
             self._process_joint_recurse(hips)
-        self._add_footer()
+        self._add_footers()
 
-    def _add_header(self):
+    def _add_headers(self):
+        for out in self.outs:
+            self._add_header(out)
+
+    def _add_header(self, out):
         width = self.args.spacing * 6 + self.args.radius * 6
         height = self.args.spacing * 2 + self.args.radius * 2
-        self.out.write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n')
-        self.out.write('<g>\n')
-        self.out.write('<rect width="%d" height="%d" fill="white" />\n' % (
+        out.write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n')
+        out.write('<g>\n')
+        out.write('<rect width="%d" height="%d" fill="white" />\n' % (
                 width, height))
         for dimension_plot in self.dimension_plots.values():
-            self.out.write(
+            out.write(
                 '<circle cx="%d" cy="%d" r="%d" fill="none" stroke="#f0f0f0" stroke-width="1" />\n' % (
                     dimension_plot["cx"], dimension_plot["cy"], self.args.radius))
 
-    def _add_footer(self):
-        self.out.write('</g>\n')
-        self.out.write('</svg>\n')
+    def _add_footers(self):
+        for out in self.outs:
+            self._add_footer(out)
+
+    def _add_footer(self, out):
+        out.write('</g>\n')
+        out.write('</svg>\n')
 
     def _process_joint_recurse(self, joint):
-        if joint.rotation_definition and joint.name == self.args.joint:
-            self._process_rotation_definition(joint.rotation_definition)
+        if joint.rotation_definition:
+            self._process_rotation_definition(joint)
         for child in joint.children:
             self._process_joint_recurse(child)
 
-    def _process_rotation_definition(self, rotation_definition):
-        for channel, degrees in rotation_definition:
-            self._plot_angle(channel, degrees)
+    def _process_rotation_definition(self, joint):
+        out = self.outs[joint.index_with_rotation]
+        for channel, degrees in joint.rotation_definition:
+            self._plot_angle(out, channel, degrees)
 
-    def _plot_angle(self, channel, degrees):
+    def _plot_angle(self, out, channel, degrees):
         dimension_plot = self.dimension_plots[channel]
-        self.out.write(
+        out.write(
             '<circle cx="%d" cy="%d" r="%d" fill="black" fill-opacity="0.1"/>\n' % (
                 dimension_plot["cx"] + self.args.radius * math.cos(math.radians(degrees)),
                 dimension_plot["cy"] + self.args.radius * math.sin(math.radians(degrees)),
