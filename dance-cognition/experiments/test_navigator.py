03ef35d5f70b2fb8d53f215ebd7467798d0274bd
Alex Berman <alex@nimations.com>
2013-12-07 11:26:20 +0100

    refactoring

--- dance-cognition/experiments/test_navigator.py
+++ dance-cognition/experiments/test_navigator.py
@@ -15,11 +15,11 @@ from stopwatch import Stopwatch
 FRAME_RATE = 50
 
 class MapView(QtOpenGL.QGLWidget):
-    def __init__(self, parent):
+    def __init__(self, parent, experiment):
         QtOpenGL.QGLWidget.__init__(self, parent)
+        self._experiment = experiment
 
     def render(self):
-        self._main_window = self.parent()
         self._render_map()
         self._render_path()
         self._render_traveller()
@@ -28,7 +28,7 @@ class MapView(QtOpenGL.QGLWidget):
         glColor3f(0, 1, 0)
         glPointSize(3.0)
         glBegin(GL_POINTS)
-        for x,y in self._main_window.map_points:
+        for x,y in self._experiment.map_points:
             glVertex2f(*self._vertex(x, y))
         glEnd()
 
@@ -38,8 +38,8 @@ class MapView(QtOpenGL.QGLWidget):
     def _render_path(self):
         glColor3f(0.5, 0.5, 1.0)
 
-        departure_x, departure_y = self._main_window.path[0]
-        destination_x, destination_y = self._main_window.path[-1]
+        departure_x, departure_y = self._experiment.path[0]
+        destination_x, destination_y = self._experiment.path[-1]
         glLineWidth(1.0)
         glRectf(departure_x*self.width-3,
                 departure_y*self.height-3,
@@ -53,12 +53,12 @@ class MapView(QtOpenGL.QGLWidget):
 
         glPointSize(3.0)
         glBegin(GL_POINTS)
-        for x,y in self._main_window.path[1:-1]:
+        for x,y in self._experiment.path[1:-1]:
             glVertex2f(*self._vertex(x, y))
         glEnd()
 
         glBegin(GL_LINE_STRIP)
-        for x,y in self._main_window.path:
+        for x,y in self._experiment.path:
             glVertex2f(*self._vertex(x, y))
         glEnd()
 
@@ -67,7 +67,7 @@ class MapView(QtOpenGL.QGLWidget):
         glPointSize(3.0)
         glBegin(GL_POINTS)
         glVertex2f(*self._vertex(
-                *self._main_window.path_follower.current_position()))
+                *self._experiment.path_follower.current_position()))
         glEnd()
 
     def initializeGL(self):
@@ -101,9 +101,10 @@ class MapView(QtOpenGL.QGLWidget):
         return QtCore.QSize(500, 500)
 
 class MainWindow(QtGui.QMainWindow):
-    def __init__(self):
+    def __init__(self, experiment):
         QtGui.QMainWindow.__init__(self)
-        self._map_view = MapView(self)
+        self._experiment = experiment
+        self._map_view = MapView(self, experiment)
         self.setCentralWidget(self._map_view)
         self._create_menu()
         self._generate_map_and_path()
@@ -123,21 +124,33 @@ class MainWindow(QtGui.QMainWindow):
     def _add_generate_path_action(self):
         action = QtGui.QAction('Generate &path', self)
         action.setShortcut('Ctrl+P')
-        action.triggered.connect(self._generate_path)
-        self._menu.addAction(action)        
+        action.triggered.connect(self._experiment.generate_path)
+        self._menu.addAction(action)
 
     def _add_generate_map_action(self):
         action = QtGui.QAction('Generate &map', self)
         action.setShortcut('Ctrl+M')
         action.triggered.connect(self._generate_map_and_path)
-        self._menu.addAction(action)        
+        self._menu.addAction(action)
 
     def _generate_map_and_path(self):
-        self._generate_map()
-        self._create_navigator()
-        self._generate_path()
+        self._experiment.generate_map()
+        self._experiment.create_navigator()
+        self._experiment.generate_path()
 
-    def _generate_map(self):
+    def _update(self):
+        self.now = self.stopwatch.get_elapsed_time()
+        if self._frame_count == 0:
+            self.stopwatch.start()
+        else:
+            time_increment = self.now - self.previous_frame_time
+            self._experiment.proceed(time_increment)
+            self._map_view.updateGL()
+        self.previous_frame_time = self.now
+        self._frame_count += 1
+
+class Experiment:
+    def generate_map(self):
         samples, labels = make_classification(
             n_features=2, n_redundant=0, n_informative=1,
             n_clusters_per_class=1, n_samples=1000)
@@ -148,28 +161,21 @@ class MainWindow(QtGui.QMainWindow):
         max_value = max([max(point) for point in points])
         return (points - min_value) / (max_value - min_value)
 
-    def _create_navigator(self):
+    def create_navigator(self):
         self._navigator = Navigator(map_points=self.map_points)
 
-    def _generate_path(self):
+    def generate_path(self):
         departure = random.choice(self.map_points)
         destination = random.choice(self.map_points)
         self.path = self._navigator.generate_path(
             departure=departure, destination=destination, resolution=10)
         self.path_follower = PathFollower(self.path, duration=5.0)
 
-    def _update(self):
-        self.now = self.stopwatch.get_elapsed_time()
-        if self._frame_count == 0:
-            self.stopwatch.start()
-        else:
-            time_increment = self.now - self.previous_frame_time
-            self.path_follower.proceed(time_increment)
-            self._map_view.updateGL()
-        self.previous_frame_time = self.now
-        self._frame_count += 1
+    def proceed(self, time_increment):
+        self.path_follower.proceed(time_increment)
 
+experiment = Experiment()
 app = QtGui.QApplication(sys.argv)
-window = MainWindow()
+window = MainWindow(experiment)
 window.show()
 app.exec_()
