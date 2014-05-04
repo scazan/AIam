f4d1d58da025d41071002294db539cc071143cdb
Alex Berman <alex@nimations.com>
2014-05-04 13:36:50 +0200

    plot_observations: support for automatically splitting output into segments, in order to recover segmentation that was lost when BVH files were appended

--- dance-cognition/experiments/plot_observations.py
+++ dance-cognition/experiments/plot_observations.py
@@ -11,14 +11,29 @@
 
 from argparse import ArgumentParser
 from storage import load_model
+import numpy
 
 parser = ArgumentParser()
 parser.add_argument("model")
 parser.add_argument("--output", "-o", default="observations.dat")
+parser.add_argument("--split-sensitivity", type=float)
 args = parser.parse_args()
 model = load_model(args.model)[0]
 
+num_segments = 1
 out = open(args.output, "w")
+previous_observation = None
 for observation in model.normalized_observed_reductions:
+    if previous_observation is not None and args.split_sensitivity and \
+            numpy.linalg.norm(observation - previous_observation) > args.split_sensitivity:
+        print >>out
+        num_segments += 1
     print >>out, " ".join([str(value) for value in observation])
+    previous_observation = observation
 out.close()
+
+if args.split_sensitivity:
+    print "Plotted %d observations in %d segments" % (
+        len(model.normalized_observed_reductions), num_segments)
+else:
+    print "Plotted %d observations" % len(model.normalized_observed_reductions)
