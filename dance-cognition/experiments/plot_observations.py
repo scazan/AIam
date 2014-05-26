3f1db889cdc0220a5ea1d13a05e3a33142900365
Alex Berman <alex@nimations.com>
2014-05-26 11:41:04 +0200

    plot_observations.py: support for generating separate plot for each segment

--- dance-cognition/experiments/plot_observations.py
+++ dance-cognition/experiments/plot_observations.py
@@ -17,23 +17,36 @@ parser = ArgumentParser()
 parser.add_argument("model")
 parser.add_argument("--output", "-o", default="observations.dat")
 parser.add_argument("--split-sensitivity", type=float)
+parser.add_argument("--segments-output")
 args = parser.parse_args()
 model = load_model(args.model)[0]
 
-num_segments = 1
-out = open(args.output, "w")
+segments = []
+segment = []
 previous_observation = None
+out = open(args.output, "w")
 for observation in model.normalized_observed_reductions:
     if previous_observation is not None and args.split_sensitivity and \
             numpy.linalg.norm(observation - previous_observation) > args.split_sensitivity:
+        segments.append(segment)
+        segment = []
         print >>out
-        num_segments += 1
-    print >>out, " ".join([str(value) for value in observation])
+    segment.append(observation)
     previous_observation = observation
+    print >>out, " ".join([str(value) for value in observation])
 out.close()
+
+if args.segments_output:
+    n = 0
+    for segment in segments:
+        out = open(args.segments_output % n, "w")
+        for observation in segment:
+            print >>out, " ".join([str(value) for value in observation])
+        out.close()
+        n += 1
 
 if args.split_sensitivity:
     print "Plotted %d observations in %d segments" % (
-        len(model.normalized_observed_reductions), num_segments)
+        len(model.normalized_observed_reductions), len(segments))
 else:
     print "Plotted %d observations" % len(model.normalized_observed_reductions)
