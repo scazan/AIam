bde1874afc4c5a7efc695f7c64a74fbb59df15a5
Alex Berman <alex@nimations.com>
2014-11-18 09:52:01 +0100

    added logging in websocket_client.js

--- dance-cognition/experiments/dimensionality_reduction/html5/websocket_client.js
+++ dance-cognition/experiments/dimensionality_reduction/html5/websocket_client.js
@@ -2,8 +2,17 @@ function WebsocketClient(address, handleEvent) {
     this.ws = new WebSocket(address);
 
     this.ws.onopen = function(_event) {
+	console.log("WebSocket connection established");
 	var event = new Event("SUBSCRIBE", ["PARAMETER"]);
 	this.send(packEvent(event));
+    }
+
+    this.ws.onerror = function() {
+	console.log("WebSocket.onerror");
+    }
+
+    this.ws.onclose = function(event) {
+	console.log("WebSocket.onclose: " + event.reason + " (code " + event.code + ")");
     }
 
     this.ws.onmessage = function(message) {
