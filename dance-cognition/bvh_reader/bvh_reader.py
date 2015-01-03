scazan/AIam

commit 38a1445887aefa6f784743a6ed9f1ce96f6a1f9a

continued refactoring started in ffc9db0595c0e678296e40055107b85f98e6fb21

dance-cognition/bvh_reader/bvh_reader.py
@@ -27,7 +27,6 @@ class JointDefinition:
         self.index = index
         self.child_definitions = []
         self.channels = []
-        self.translation = array([0.,0.,0.])
         self.translation_matrix = array([
             [0.,0.,0.,0.],
             [0.,0.,0.,0.],
@@ -64,6 +63,7 @@ class Joint:
     def __init__(self, definition, parent=None):
         self.definition = definition
         self.parent = parent
+        self.translation = array([0.,0.,0.])
         self.children = []
 
     def add_child(self, child):
@@ -124,14 +124,11 @@ class Hierarchy:
         joint_definition = JointDefinition(name, self._joint_index)
         self._joint_index += 1
         joint_definition.channels = node.channels
-        joint_definition.translation[0] = node.offset[0]
-        joint_definition.translation[1] = node.offset[1]
-        joint_definition.translation[2] = node.offset[2]
 
         joint_definition.translation_matrix = make_translation_matrix(
-            joint_definition.translation[0],
-            joint_definition.translation[1],
-            joint_definition.translation[2])
+            node.offset[0],
+            node.offset[1],
+            node.offset[2])
 
         if "Xrotation" in node.channels:
             joint_definition.rotation_channels = filter(
@@ -173,10 +170,10 @@ class Hierarchy:
             frame_data_index += 1
 
         if "Xposition" in frame_dict:
-            joint.translation_matrix = make_translation_matrix(
-                frame_dict["Xposition"],
-                frame_dict["Yposition"],
-                frame_dict["Zposition"])
+            joint.translation = array([
+                    frame_dict["Xposition"],
+                    frame_dict["Yposition"],
+                    frame_dict["Zposition"]])
 
         if joint.definition.has_rotation:
             joint.angles = [radians(frame_dict[channel])
@@ -198,7 +195,8 @@ class Hierarchy:
             parent_trtr = joint.parent.trtr
             localtoworld = dot(parent_trtr, joint.definition.translation_matrix)
         else:
-            localtoworld = dot(joint.definition.translation_matrix, joint.translation_matrix)
+            translation_matrix = make_translation_matrix(*joint.translation)
+            localtoworld = dot(joint.definition.translation_matrix, translation_matrix)
 
         if joint.definition.has_rotation:
             if joint.definition.has_static_rotation:

dance-cognition/experiments/entities/hierarchical.py
@@ -1,6 +1,5 @@
 from experiment import *
 from angle_parameters import EulerTo3Vectors, EulerToQuaternion
-from bvh_reader.geo import *
 from numpy import array, dot
 from transformations import euler_matrix
 import random
@@ -123,8 +122,7 @@ class Entity(BaseEntity):
             weighted_vector = parameters[parameter_index:parameter_index+3]
             parameter_index += 3
             normalized_vector = numpy.array(weighted_vector) / self.args.translation_weight
-            translation = self.bvh_reader.skeleton_scale_vector(normalized_vector)
-            joint.translation_matrix = make_translation_matrix(*translation)
+            joint.translation = self.bvh_reader.skeleton_scale_vector(normalized_vector)
 
         if joint.definition.has_rotation and not joint.definition.has_static_rotation:
             rotation_parameters = parameters[

commit 030d76eb7c04739696495ce2224f9c01e1590026

fixed export bug introduced in 38a1445887aefa6f784743a6ed9f1ce96f6a1f9a

dance-cognition/bvh_reader/bvh_reader.py
@@ -125,6 +125,7 @@ class Hierarchy:
         self._joint_index += 1
         joint_definition.channels = node.channels
 
+        joint_definition.offset = node.offset
         joint_definition.translation_matrix = make_translation_matrix(
             node.offset[0],
             node.offset[1],

dance-cognition/bvh_writer.py
@@ -39,9 +39,9 @@ class BvhWriter:
 
         self._write_indent()
         self._write("OFFSET\t%.4f\t%.4f\t%.4f\n" % (
-                joint_definition.translation[0],
-                joint_definition.translation[1],
-                joint_definition.translation[2]))
+                joint_definition.offset[0],
+                joint_definition.offset[1],
+                joint_definition.offset[2]))
 
         if len(joint_definition.channels) > 0:
             self._write_indent()