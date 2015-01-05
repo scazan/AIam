4d49c6d19b79ae89a3adb1dd576c7b342a02ef13
Alex Berman <alex@nimations.com>
2015-01-05 09:11:56 +0100

    added unit test for EulerToQuaternion

--- dance-cognition/experiments/test_angle_parameters.py
+++ dance-cognition/experiments/test_angle_parameters.py
@@ -1,9 +1,15 @@
 from angle_parameters import *
 import unittest
 import math
+from transformations import euler_matrix, is_same_transform
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/..")
+from bvh_reader.geo import Euler
 
 class AngleParametersTest(unittest.TestCase):
-    def test_radians_to_vector2d_and_back(self):
+    def test_radians_vector2d_inversion(self):
         for n in range(100):
             r = float(n) / 100 * (math.pi*2)
             self._assert_radians_equal(r, vector2d_to_radians(radians_to_vector2d(r)))
@@ -20,19 +26,26 @@ class AngleParametersTest(unittest.TestCase):
             r -= math.pi*2
         return r
 
-    def test_euler_to_vector6d_and_back(self):
+    def test_euler_to_3vectors_inversion(self):
+        self._test_angle_3dof_inversion(EulerTo3Vectors)
+
+    def test_euler_to_quaternion_inversion(self):
+        self._test_angle_3dof_inversion(EulerToQuaternion)
+
+    def _test_angle_3dof_inversion(self, parameterization):
         eulers = [
             (0.1, 0.3, 1.8),
             (4.1, 2.3, 3.8),
             (5.1, 0.1, 2.8),
             ]
-        for euler in eulers:
-            self._assert_euler_almost_equals(
-                euler, 
-                vector6d_to_euler(euler_to_vector6d(*euler)))
+        for angles in eulers:
+            euler = Euler(angles)
+            parameters = parameterization.rotation_to_parameters(euler)
+            self._assert_euler_equals(
+                euler.angles,
+                parameterization.parameters_to_rotation(parameters, euler.axes))
 
-    def _assert_euler_almost_equals(self, x, y):
-        self._assert_radians_equal(x[0], y[0])
-        self._assert_radians_equal(x[1], y[1])
-        self._assert_radians_equal(x[2], y[2])
-
+    def _assert_euler_equals(self, angles1, angles2):
+        matrix1 = euler_matrix(*angles1)
+        matrix2 = euler_matrix(*angles2)
+        self.assertTrue(is_same_transform(matrix1, matrix2))
