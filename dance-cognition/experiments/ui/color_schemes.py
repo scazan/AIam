c703fa953941b6e352c9c3acdd5ff6747933fbc7
Alex Berman <alex@nimations.com>
2015-01-22 12:07:10 +0100

    trying to fix ugly floor corners

--- dance-cognition/experiments/ui/color_schemes.py
+++ dance-cognition/experiments/ui/color_schemes.py
@@ -5,7 +5,7 @@ color_schemes = OrderedDict()
 color_schemes["white"] = {
     "background": (1.0, 1.0, 1.0, 0.0),
     "unit_cube": (0, 0, 0, 0.2),
-    "floor": (0, 0, 0, 0.2),
+    "floor": (.2, .2, .2),
     "focus": (0, 0, 0, 0.2),
     "input": (0, 1, 0),
     "output": (0, 0, 0),
@@ -14,7 +14,7 @@ color_schemes["white"] = {
 color_schemes["black"] = {
     "background": (0.0, 0.0, 0.0, 0.0),
     "unit_cube": (1, 1, 1, 0.2),
-    "floor": (1, 1, 1, 0.2),
+    "floor": (.2, .2, .2),
     "focus": (1, 1, 1, 0.2),
     "input": (0, 1, 0),
     "output": (1, 1, 1),
