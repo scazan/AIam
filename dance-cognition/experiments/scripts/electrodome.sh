14984f411f7077b3c5bc825e745fd63f7b1cbde7
Alex Berman <alex@nimations.com>
2015-01-22 15:03:41 +0100

    added logo image

--- dance-cognition/experiments/scripts/electrodome.sh
+++ dance-cognition/experiments/scripts/electrodome.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python dim_reduce.py -p valencia_quaternion_7d_friction --mode=improvise --websockets --no-toolbar --fullscreen --color-scheme=black -frame-rate=50
+python dim_reduce.py -p valencia_quaternion_7d_friction --mode=improvise --websockets --no-toolbar --fullscreen --color-scheme=black -frame-rate=50 --image=dimensionality_reduction/ui/assets/electrodome_logo.png --image-scale=0.8
