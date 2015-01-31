49cbd97ce75ec01b0f54d769c0b1e0d953c101a7
Alex Berman <alex@nimations.com>
2015-01-31 14:25:36 +0100

    refactored plot_observations.py

--- dance-cognition/experiments/plot_observations.py
+++ dance-cognition/experiments/plot_observations.py
@@ -18,6 +18,7 @@ parser.add_argument("model")
 parser.add_argument("--output", "-o", default="observations.dat")
 parser.add_argument("--split-sensitivity", type=float)
 parser.add_argument("--segments-output")
+parser.add_argument("--type", choices=["gnuplot", "svg"], default="gnuplot")
 args = parser.parse_args()
 model = load_model(args.model)[0]
 
@@ -40,21 +41,27 @@ if args.split_sensitivity:
 else:
     segments = [model.normalized_observed_reductions]
 
-out = open(args.output, "w")
-for observations in segments:
-    for observation in observations:
-        print >>out, " ".join([str(value) for value in observation])
-    print >>out
-out.close()
+class gnuplotGenerator:
+    def generate(self, segments, out):
+        for observations in segments:
+            for observation in observations:
+                print >>out, " ".join([str(value) for value in observation])
+            print >>out
 
-if args.segments_output:
-    n = 0
-    for segment in segments:
-        out = open(args.segments_output % n, "w")
-        for observation in segment:
-            print >>out, " ".join([str(value) for value in observation])
-        out.close()
-        n += 1
+        global args
+        if args.segments_output:
+            n = 0
+            for segment in segments:
+                out = open(args.segments_output % n, "w")
+                for observation in segment:
+                    print >>out, " ".join([str(value) for value in observation])
+                out.close()
+                n += 1
+
+generator = eval("%sGenerator" % args.type)()
+out = open(args.output, "w")
+generator.generate(segments, out)
+out.close()
 
 if args.split_sensitivity:
     print "Plotted %d observations in %d segments" % (
