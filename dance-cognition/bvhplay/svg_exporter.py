9077fa16555119d4e585345675d1447e85307889
Alex Berman <alex@nimations.com>
2015-02-08 09:05:06 +0100

    render_pose_map_contents.py: whole grid, but strange problems in one corner

--- dance-cognition/bvhplay/svg_exporter.py
+++ dance-cognition/bvhplay/svg_exporter.py
@@ -9,7 +9,8 @@ class SvgExporter:
         self._floor_y = None
 
     def export_frame(self, svg_file, t, width, height,
-                     opacity=1, x_offset=0, constrain_floor=False, auto_crop=False, stroke_width=1):
+                     opacity=1, x=0, y=0,
+                     constrain_floor=False, auto_crop=False, stroke_width=1):
         self._skeleton.populate_skelscreenedges(self._skelscreenedges, t)
 
         if constrain_floor:
@@ -39,10 +40,10 @@ class SvgExporter:
             sv1x, sv1y = sv1
             sv2x, sv2y = sv2
             svg_file.write('<line x1="%s" y1="%s" x2="%s" y2="%s" style="stroke:black;fill:none;stroke-width:%f;stroke-opacity:%f" />' % (
-                sv1x * width + x_offset,
-                sv1y * height,
-                sv2x * width + x_offset,
-                sv2y * height,
+                sv1x * width + x,
+                sv1y * height + y,
+                sv2x * width + x,
+                sv2y * height + y,
                 stroke_width,
                 opacity))
 
@@ -53,7 +54,12 @@ class SvgExporter:
         max_y = max([max(v1[1], v2[1]) for v1, v2 in vertices])
         range_x = max_x - min_x
         range_y = max_y - min_y
-        scale = 1. / max(range_x, range_y)
+        range_max = max(range_x, range_y)
+        if range_max == 0:
+            scale = 1
+            print "WARNING: range_max=0"
+        else:
+            scale = 1. / range_max
         translate_x = .5 - range_x * scale / 2
         translate_y = .5 - range_y * scale / 2
         return [
--- dance-cognition/experiments/render_pose_map_contents.py
+++ dance-cognition/experiments/render_pose_map_contents.py
@@ -14,6 +14,7 @@ parser.add_argument("--plot-size", type=float, default=500)
 parser.add_argument("--camera-x", "-cx", type=float, default=0)
 parser.add_argument("--camera-y", "-cy", type=float, default=10)
 parser.add_argument("--camera-z", "-cz", type=float, default=220)
+parser.add_argument("--grid-resolution", type=int, default=10)
 
 class PoseMapRenderer:
     _bvh_tempfile_path = "_temp.bvh"
@@ -25,37 +26,52 @@ class PoseMapRenderer:
     def render(self):
         self._experiment.bvh_reader.set_pose_from_time(self._experiment.pose, 0) # hack (should not be needed)
         self._out = open(self._args.output, "w")
+        self._cell_size = self._args.plot_size / self._args.grid_resolution
         self._generate_header()
-        normalized_reduction = [0.5] * self._experiment.student.n_components
-        reduction = self._experiment.student.unnormalize_reduction(normalized_reduction)
-        output = self._experiment.student.inverse_transform(numpy.array([reduction]))[0]
-        self._experiment.entity.parameters_to_processed_pose(output, self._experiment.pose)
-        self._render_pose(self._experiment.pose, self._args.plot_size/2, self._args.plot_size/2)
+        for grid_y in xrange(self._args.grid_resolution):
+            for grid_x in xrange(self._args.grid_resolution):
+                self._render_cell(grid_x, grid_y)
         self._generate_footer()
         self._out.close()
 
-    def _render_pose(self, pose, cx, cy):
-        self._save_pose_as_temp_bvh(pose)
-        self._export_temp_bvh_to_svg(cx, cy)
+    def _render_cell(self, grid_x, grid_y):
+        px = float(grid_x) / self._args.grid_resolution * self._args.plot_size
+        py = float(grid_y) / self._args.grid_resolution * self._args.plot_size
+        self._get_pose_in_cell(grid_x, grid_y)
+        self._render_pose_in_position(px, py)
+
+    def _get_pose_in_cell(self, grid_x, grid_y):
+        normalized_reduction = [0.5] * self._experiment.student.n_components
+        normalized_reduction[0] = float(grid_x) / (self._args.grid_resolution - 1)
+        normalized_reduction[1] = float(grid_y) / (self._args.grid_resolution - 1)
+        reduction = self._experiment.student.unnormalize_reduction(normalized_reduction)
+        output = self._experiment.student.inverse_transform(numpy.array([reduction]))[0]
+        self._experiment.entity.parameters_to_processed_pose(output, self._experiment.pose)
+
+    def _render_pose_in_position(self, px, py):
+        self._save_pose_as_temp_bvh()
+        self._export_temp_bvh_to_svg(px, py)
         self._delete_temp_bvh()
 
-    def _save_pose_as_temp_bvh(self, pose):
+    def _save_pose_as_temp_bvh(self):
         bvh_writer = BvhWriter(
             self._experiment.bvh_reader.get_hierarchy(),
             self._experiment.bvh_reader.get_frame_time())
-        frame = self._experiment.pose_to_bvh_frame(pose)
+        frame = self._experiment.pose_to_bvh_frame(self._experiment.pose)
         bvh_writer.add_frame(frame)
         bvh_writer.write(self._bvh_tempfile_path)
 
-    def _export_temp_bvh_to_svg(self, cx, cy):
+    def _export_temp_bvh_to_svg(self, px, py):
         bvh_to_svg_exporter = SvgExporter(
             self._bvh_tempfile_path,
             self._args.camera_x, self._args.camera_y, self._args.camera_z)
         bvh_to_svg_exporter.export_frame(
             self._out,
             t=0,
-            width=self._args.plot_size,
-            height=self._args.plot_size,
+            x=px,
+            y=py,
+            width=self._cell_size,
+            height=self._cell_size,
             auto_crop=True)
 
     def _delete_temp_bvh(self):
