dab90dc89f992505166ce00162d44ed9d8681e7e
Alex Berman <alex@nimations.com>
2015-02-08 07:38:11 +0100

    refactored export_svg.py by extracting new module svg_exporter.py

--- dance-cognition/bvhplay/export_svg.py
+++ dance-cognition/bvhplay/export_svg.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from skeleton import process_bvhfile
-from camera import Camera
+from svg_exporter import SvgExporter
 import math
 from argparse import ArgumentParser
 import os
@@ -25,64 +24,6 @@ parser.add_argument("--constrain-floor", action="store_true")
 parser.add_argument("--auto-crop", action="store_true")
 parser.add_argument("--split", action="store_true")
 args = parser.parse_args()
-
-skeleton = process_bvhfile(args.filename)
-camera = Camera(args.camera_x, args.camera_y, args.camera_z, cfx=20, ppdist=30)
-skelscreenedges = skeleton.make_skelscreenedges()
-
-def export_frame(t, opacity, x_offset):
-    global args, floor_y
-
-    skeleton.populate_skelscreenedges(skelscreenedges, t)
-
-    if args.constrain_floor:
-        frame_bottom_y = min([
-                min(screenedge.sv1.tr[1], screenedge.sv2.tr[1])
-                for screenedge in skelscreenedges])
-        if floor_y is None:
-            floor_y = frame_bottom_y
-        y_offset = floor_y - frame_bottom_y
-
-        for screenedge in skelscreenedges:
-            screenedge.sv1.tr[1] += y_offset
-            screenedge.sv2.tr[1] += y_offset
-
-    screen_vertices = []
-    for screenedge in skelscreenedges:
-        screenedge.worldtocam(camera)
-        screenedge.camtoscreen(camera, 1., 1.)
-        sv1 = (screenedge.sv1.screenx, screenedge.sv1.screeny)
-        sv2 = (screenedge.sv2.screenx, screenedge.sv2.screeny)
-        screen_vertices.append((sv1, sv2))
-
-    if args.auto_crop:
-        screen_vertices = auto_crop(screen_vertices)
-
-    for sv1, sv2 in screen_vertices:
-        sv1x, sv1y = sv1
-        sv2x, sv2y = sv2
-        write_svg('<line x1="%s" y1="%s" x2="%s" y2="%s" style="stroke:black;fill:none;stroke-width:%f;stroke-opacity:%f" />' % (
-            sv1x * args.frame_width + x_offset,
-            sv1y * args.frame_height,
-            sv2x * args.frame_width + x_offset,
-            sv2y * args.frame_height,
-            args.stroke_width,
-            opacity))
-
-def auto_crop(vertices):
-    min_x = min([min(v1[0], v2[0]) for v1, v2 in vertices])
-    max_x = max([max(v1[0], v2[0]) for v1, v2 in vertices])
-    min_y = min([min(v1[1], v2[1]) for v1, v2 in vertices])
-    max_y = max([max(v1[1], v2[1]) for v1, v2 in vertices])
-    range_x = max_x - min_x
-    range_y = max_y - min_y
-    scale = 1. / max(range_x, range_y)
-    translate_x = .5 - range_x * scale / 2
-    translate_y = .5 - range_y * scale / 2
-    return [
-        ((scale * (v1[0] - min_x) + translate_x, scale * (v1[1] - min_y) + translate_y),
-         (scale * (v2[0] - min_x) + translate_x, scale * (v2[1] - min_y) + translate_y))
-        for v1, v2 in vertices]
 
 def write_svg(string):
     global output
@@ -113,9 +54,6 @@ def write_header():
 def write_footer():
     write_svg('</svg>')
 
-if args.constrain_floor:
-    floor_y = None
-
 def opacity(n, relative_frame_index):
     if args.split:
         return 1
@@ -134,6 +72,8 @@ if not args.split:
     output = open(output_path, "w")
     write_header()
 
+exporter = SvgExporter(args.filename, args.camera_x, args.camera_y, args.camera_z)
+
 for n in range(num_frames):
     relative_frame_index = float(n) / num_frames
     frame_index = begin + int(relative_frame_index * (end - begin))
@@ -145,7 +85,15 @@ for n in range(num_frames):
         output = open(output_path, "w")
         write_header()
 
-    export_frame(frame_index, opacity(n, relative_frame_index), x_offset)
+    exporter.export_frame(
+        output,
+        frame_index,
+        args.frame_width, args.frame_height,
+        opacity(n, relative_frame_index),
+        x_offset,
+        args.constrain_floor,
+        args.auto_crop,
+        args.stroke_width)
 
     if args.split:
         write_footer()
---
+++ dance-cognition/bvhplay/svg_exporter.py
@@ -0,0 +1,62 @@
+from skeleton import process_bvhfile
+from camera import Camera
+
+class SvgExporter:
+    def __init__(self, bvh_filename, camera_x, camera_y, camera_z):
+        self._skeleton = process_bvhfile(bvh_filename)
+        self._camera = Camera(camera_x, camera_y, camera_z, cfx=20, ppdist=30)
+        self._skelscreenedges = self._skeleton.make_skelscreenedges()
+        self._floor_y = None
+
+    def export_frame(self, svg_file, t, frame_width, frame_height,
+                     opacity=1, x_offset=0, constrain_floor=False, auto_crop=False, stroke_width=1):
+        self._skeleton.populate_skelscreenedges(self._skelscreenedges, t)
+
+        if constrain_floor:
+            frame_bottom_y = min([
+                    min(screenedge.sv1.tr[1], screenedge.sv2.tr[1])
+                    for screenedge in self._skelscreenedges])
+            if self._floor_y is None:
+                self._floor_y = frame_bottom_y
+            y_offset = self._floor_y - frame_bottom_y
+
+            for screenedge in self._skelscreenedges:
+                screenedge.sv1.tr[1] += y_offset
+                screenedge.sv2.tr[1] += y_offset
+
+        screen_vertices = []
+        for screenedge in self._skelscreenedges:
+            screenedge.worldtocam(self._camera)
+            screenedge.camtoscreen(self._camera, 1., 1.)
+            sv1 = (screenedge.sv1.screenx, screenedge.sv1.screeny)
+            sv2 = (screenedge.sv2.screenx, screenedge.sv2.screeny)
+            screen_vertices.append((sv1, sv2))
+
+        if auto_crop:
+            screen_vertices = self._auto_crop(screen_vertices)
+
+        for sv1, sv2 in screen_vertices:
+            sv1x, sv1y = sv1
+            sv2x, sv2y = sv2
+            svg_file.write('<line x1="%s" y1="%s" x2="%s" y2="%s" style="stroke:black;fill:none;stroke-width:%f;stroke-opacity:%f" />' % (
+                sv1x * frame_width + x_offset,
+                sv1y * frame_height,
+                sv2x * frame_width + x_offset,
+                sv2y * frame_height,
+                stroke_width,
+                opacity))
+
+    def _auto_crop(self, vertices):
+        min_x = min([min(v1[0], v2[0]) for v1, v2 in vertices])
+        max_x = max([max(v1[0], v2[0]) for v1, v2 in vertices])
+        min_y = min([min(v1[1], v2[1]) for v1, v2 in vertices])
+        max_y = max([max(v1[1], v2[1]) for v1, v2 in vertices])
+        range_x = max_x - min_x
+        range_y = max_y - min_y
+        scale = 1. / max(range_x, range_y)
+        translate_x = .5 - range_x * scale / 2
+        translate_y = .5 - range_y * scale / 2
+        return [
+            ((scale * (v1[0] - min_x) + translate_x, scale * (v1[1] - min_y) + translate_y),
+             (scale * (v2[0] - min_x) + translate_x, scale * (v2[1] - min_y) + translate_y))
+            for v1, v2 in vertices]
