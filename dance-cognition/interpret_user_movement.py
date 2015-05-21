152c55ab3eabfbbf6ea81f630a2ba231015a73ad
Alex Berman <alex@nimations.com>
2015-05-21 15:53:57 +0200

    interpreter handles multiple users (considers most active user)

--- dance-cognition/interpret_user_movement.py
+++ dance-cognition/interpret_user_movement.py
@@ -30,32 +30,64 @@ class Joint:
         self._previous_time = now
         self._previous_position = new_position
 
-class UserMovementInterpreter:
-    def __init__(self):
+class User:
+    def __init__(self, interpreter):
+        self._interpreter = interpreter
         self._joints = {"left_hand": Joint(),
                         "right_hand": Joint()}
+        self._num_updated_joints = 0
+        self._num_received_frames = 0
+        self._activity = 0
 
-    def handle_joint_data(self, user_id, joint_name, x, y, z):
+    def handle_joint_data(self, joint_name, x, y, z):
         joint = self._joints[joint_name]
         joint.update(x, y, z)
-        if joint.get_activity() is not None:
-            if joint_name == "right_hand":
-                average_activity = sum([joint.get_activity() for joint in self._joints.values()]) / \
+        self._last_updated_joint = joint_name
+        self._num_updated_joints += 1
+        if self._num_updated_joints == len(self._joints):
+            self._num_updated_joints = 0
+            self._num_received_frames += 1
+            if self._num_received_frames > 1:
+                self._activity = sum([joint.get_activity() for joint in self._joints.values()]) / \
                     len(self._joints)
-                # preferred_distance = novelty = average_activity * .02
-                # websocket_client.send_event(
-                #     Event(Event.PARAMETER,
-                #           {"name": "preferred_distance",
-                #            "value": preferred_distance}))
-                # websocket_client.send_event(
-                #     Event(Event.PARAMETER,
-                #           {"name": "novelty",
-                #            "value": novelty}))
-                velocity = average_activity * .02
-                websocket_client.send_event(
-                    Event(Event.PARAMETER,
-                          {"name": "velocity",
-                           "value": velocity}))
+                self._interpreter.user_has_new_information()
+
+    def get_activity(self):
+        return self._activity
+    
+class UserMovementInterpreter:
+    def __init__(self):
+        self._users = {}
+
+    def handle_joint_data(self, user_id, joint_name, x, y, z):
+        if user_id not in self._users:
+            self._users[user_id] = User(self)
+        user = self._users[user_id]
+        user.handle_joint_data(joint_name, x, y, z)
+
+    def user_has_new_information(self):
+        self._interpret_current_state()
+        self._send_interpretation()
+
+    def _interpret_current_state(self):
+        user_activities = [user.get_activity() for user in self._users.values()]
+        self._highest_user_activity = max(user_activities)
+
+    def _send_interpretation(self):
+        # preferred_distance = novelty = average_activity * .02
+        # websocket_client.send_event(
+        #     Event(Event.PARAMETER,
+        #           {"name": "preferred_distance",
+        #            "value": preferred_distance}))
+        # websocket_client.send_event(
+        #     Event(Event.PARAMETER,
+        #           {"name": "novelty",
+        #            "value": novelty}))
+        velocity = self._highest_user_activity * .02
+        websocket_client.send_event(
+            Event(Event.PARAMETER,
+                  {"name": "velocity",
+                   "value": velocity}))
 
 interpreter = UserMovementInterpreter()
 
