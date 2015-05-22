6c9d789c53d9639df9b7f868197382d14b0e18a9
Alex Berman <alex@nimations.com>
2015-05-22 09:44:07 +0200

    in interpreter, replaced leaky integrator with time window

--- dance-cognition/interpret_user_movement.py
+++ dance-cognition/interpret_user_movement.py
@@ -5,29 +5,30 @@ from osc_receiver import OscReceiver
 from websocket_client import WebsocketClient
 from event_listener import EventListener
 from event import Event
-from leaky_integrator import LeakyIntegrator
 from vector import Vector3d
 import time
+import collections
 
 OSC_PORT = 15002
 WEBSOCKET_HOST = "localhost"
+WINDOW_DURATION = 1.0
+FPS = 30.0
 
 class Joint:
     def __init__(self):
-        self._previous_time = None
-        self._activity = LeakyIntegrator(response_factor=.999)
+        self._previous_position = None
+        self._buffer_size = int(WINDOW_DURATION * FPS)
+        self._activity_buffer = collections.deque(
+            [0] * self._buffer_size, maxlen=self._buffer_size)
 
     def get_activity(self):
-        return self._activity.value()
+        return sum(self._activity_buffer) / self._buffer_size
 
     def update(self, x, y, z):
-        now = time.time()
         new_position = Vector3d(x, y, z)
-        if self._previous_time is not None:
+        if self._previous_position is not None:
             movement = (new_position - self._previous_position).mag()
-            time_increment = now - self._previous_time
-            self._activity.integrate(movement, time_increment)
-        self._previous_time = now
+            self._activity_buffer.append(movement)
         self._previous_position = new_position
 
 class User:
