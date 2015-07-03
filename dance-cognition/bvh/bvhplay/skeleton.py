3bc88a6d7b79c7650c9458acc5174b91a8abdc0d
Alex Berman <alex@nimations.com>
2015-07-03 09:27:28 +0200

    bvhplay.py back on track

--- dance-cognition/bvh/bvhplay/skeleton.py
+++ dance-cognition/bvh/bvhplay/skeleton.py
@@ -22,7 +22,7 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/../libs")
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/../../libs")
 
 from math import radians, cos, sin
 from cgkit.bvh import BVHReader
