scazan/AIam

commit 6212bc3fc1f2b3405d97b4e11c09a20707516c2f

trying to optimize interpreter by using numpy instead of Vector3d. also fixed bug which could cause an exception when interpreter was started after tracker.

dance-cognition/tracked_users_viewer.py
@@ -4,7 +4,7 @@ from OpenGL.GLU import *
 from PyQt4 import QtCore, QtGui
 import math
 import collections
-from vector import Vector3d
+import numpy
 from ui.scene import Scene
 from ui.window import Window
 text_renderer_module = __import__("text_renderer")
@@ -135,12 +135,12 @@ class TrackedUsersScene(Scene):
     def _draw_label(self, user):
         head_position = user.get_joint("head").get_position()
         glColor3f(0, 0, 0)
-        position = head_position + Vector3d(0, 140, 0)
-        self._draw_text(str(user.get_id()), 100, *position, h_align="center")
+        position = head_position + [0, 140, 0]
+        self._draw_text(str(user.get_id()), 100, position[0], position[1], position[2], h_align="center")
 
     def _draw_activity(self, user):
         head_position = user.get_joint("head").get_position()
-        position = head_position + Vector3d(150, 140, 0)
+        position = head_position + [150, 140, 0]
         activity = user.get_activity()
 
         glColor3f(.8, .8, 1)
@@ -151,7 +151,11 @@ class TrackedUsersScene(Scene):
         self._draw_solid_cube(*position, sx=70, sz=70, sy=self._activity_as_height(activity))
 
         glColor3f(.5, .5, 1)
-        self._draw_text("%.1f" % activity, 100, *(position + Vector3d(0, -100, 0)), h_align="center")
+        self._draw_text("%.1f" % activity, 100,
+                        position[0],
+                        position[1] - 100,
+                        position[2],
+                        h_align="center")
 
     def _activity_as_height(self, activity):
         return activity * 3
@@ -191,27 +195,27 @@ class TrackedUsersScene(Scene):
         for n in range(len(vertices) - 1):
             vertex1 = vertices[n]
             vertex2 = vertices[n+1]
-            if vertex1.y > self.parent().floor_y and vertex2.y > self.parent().floor_y:
+            if vertex1[1] > self.parent().floor_y and vertex2[1] > self.parent().floor_y:
                 self._set_shadow_color_by_joint(user, confidence)
-                glVertex3f(vertex1.x, self.parent().floor_y, vertex1.z)
-                glVertex3f(vertex2.x, self.parent().floor_y, vertex2.z)
+                glVertex3f(*vertex1)
+                glVertex3f(*vertex2)
         glEnd()
 
     def _split_vertices_at_floor(self, vertex1, vertex2):
         floor_y = self.parent().floor_y
-        if (vertex1.y < floor_y < vertex2.y or
-            vertex1.y > floor_y > vertex2.y):
-            floor_x = vertex1.x + (vertex2.x - vertex1.x) / (vertex2.y - vertex1.y) * (floor_y - vertex1.y)
-            floor_z = vertex1.z + (vertex2.z - vertex1.z) / (vertex2.y - vertex1.y) * (floor_y - vertex1.y)
-            return [Vector3d(vertex1.x, vertex1.y, vertex1.z),
-                    Vector3d(floor_x, floor_y,   floor_z),
-                    Vector3d(vertex2.x, vertex2.y, vertex2.z)]
+        if (vertex1[1] < floor_y < vertex2[1] or
+            vertex1[1] > floor_y > vertex2[1]):
+            floor_x = vertex1[0] + (vertex2[0] - vertex1[0]) / (vertex2[1] - vertex1[1]) * (floor_y - vertex1[1])
+            floor_z = vertex1[2] + (vertex2[2] - vertex1[2]) / (vertex2[1] - vertex1[1]) * (floor_y - vertex1[1])
+            return [vertex1,
+                    numpy.array([floor_x, floor_y, floor_z]),
+                    vertex2]
         else:
             return [vertex1, vertex2]
 
     def _set_color_by_joint(self, user, confidence, vertex1, vertex2):
         a = 1 - (.8 - confidence * .8)
-        if vertex1.y <= self.parent().floor_y and vertex2.y <= self.parent().floor_y:
+        if vertex1[1] <= self.parent().floor_y and vertex2[1] <= self.parent().floor_y:
             r, g, b = SKELETON_COLOR_BELOW_FLOOR
         else:
             if user == self._selected_user:

dance-cognition/interpret_user_movement.py
@@ -14,7 +14,6 @@ from osc_receiver import OscReceiver
 from websocket_client import WebsocketClient
 from event_listener import EventListener
 from event import Event
-from vector import Vector3d
 from tracked_users_viewer import TrackedUsersViewer
 from transformations import rotation_matrix
 
@@ -55,9 +54,9 @@ class Joint:
         return sum(self._activity_buffer) / self._buffer_size
 
     def set_position(self, x, y, z):
-        new_position = Vector3d(x, y, z)
+        new_position = numpy.array([x, y, z])
         if self._previous_position is not None:
-            movement = (new_position - self._previous_position).mag()
+            movement = numpy.linalg.norm(new_position - self._previous_position)
             self._activity_buffer.append(movement)
         self._previous_position = new_position
 
@@ -156,10 +155,12 @@ class UserMovementInterpreter:
                        "joint_data": []}
 
     def handle_joint_data(self, user_id, joint_name, x, y, z, confidence):
-        self._frame["joint_data"].append((user_id, joint_name, x, y, z, confidence))
+        if self._frame is not None:
+            self._frame["joint_data"].append((user_id, joint_name, x, y, z, confidence))
 
     def handle_state(self, user_id, state):
-        self._frame["states"].append((user_id, state))
+        if self._frame is not None:
+            self._frame["states"].append((user_id, state))
 
     def _process_frame(self):
         self._tracker_rotation_matrix = rotation_matrix(
@@ -186,12 +187,12 @@ class UserMovementInterpreter:
                 pass
 
     def adjust_tracked_position(self, position):
-        unadjusted_vector = [position.x, position.y, position.z, 1]
+        unadjusted_vector = [position[0], position[1], position[2], 1]
         rotated_vector = numpy.dot(self._tracker_rotation_matrix, unadjusted_vector)
-        adjusted_vector = Vector3d(
+        adjusted_vector = numpy.array([
             rotated_vector[0],
             rotated_vector[1] + self.tracker_y_position,
-            rotated_vector[2])
+            rotated_vector[2]])
         return adjusted_vector
 
     def get_selected_user(self):

commit 74438912385432011332380f66ba565df7d05da2

fixed bug introduced in 6212bc3fc1f2b3405d97b4e11c09a20707516c2f, causing shadow not be drawn properly

dance-cognition/tracked_users_viewer.py
@@ -209,8 +209,8 @@ class TrackedUsersScene(Scene):
             vertex2 = vertices[n+1]
             if vertex1[1] > self.parent().floor_y and vertex2[1] > self.parent().floor_y:
                 self._set_shadow_color_by_joint(user, confidence)
-                glVertex3f(*vertex1)
-                glVertex3f(*vertex2)
+                glVertex3f(vertex1[0], self.parent().floor_y, vertex1[2])
+                glVertex3f(vertex2[0], self.parent().floor_y, vertex2[2])
         glEnd()
 
     def _split_vertices_at_floor(self, vertex1, vertex2):