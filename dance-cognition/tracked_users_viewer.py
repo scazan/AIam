scazan/AIam

commit 6aa2e831c1960b39c028d2d400e14089862591cc

tracked users viewer refreshes scene automatically when moving camera

dance-cognition/tracked_users_viewer.py
@@ -305,7 +305,8 @@ class TrackedUsersScene(Scene):
             self._drag_y_previous = y
             self.updateGL()
         else:
-            Scene.mouseMoveEvent(self, event)
+            if Scene.mouseMoveEvent(self, event):
+                self.updateGL()
 
     def mouseReleaseEvent(self, event):
         self._dragging_tracker_pitch = False
@@ -477,7 +478,8 @@ class TrackedUsersViewer(Window):
             if self._fullscreen_action.isChecked():
                 self._fullscreen_action.toggle()
         else:
-            self._scene.keyPressEvent(event)
+            if self._scene.keyPressEvent(event):
+                self._scene.updateGL()
             QtGui.QWidget.keyPressEvent(self, event)
 
     def sizeHint(self):

dance-cognition/ui/scene.py
@@ -93,8 +93,10 @@ class Scene(QtOpenGL.QGLWidget):
             self._set_camera_orientation(
                 self._camera_y_orientation + self._camera_drag_speed * (x - self._drag_x_previous),
                 self._camera_x_orientation + self._camera_drag_speed * (y - self._drag_y_previous))
+            return True
         elif self._dragging_y_position:
             self._camera_position[1] += self._camera_y_speed * (y - self._drag_y_previous)
+            return True
         self._drag_x_previous = x
         self._drag_y_previous = y
 
@@ -121,18 +123,22 @@ class Scene(QtOpenGL.QGLWidget):
                 new_position[0] += self._camera_key_speed * math.cos(r)
                 new_position[2] += self._camera_key_speed * math.sin(r)
                 self._set_camera_position(new_position)
+                return True
             elif key == QtCore.Qt.Key_D:
                 new_position[0] -= self._camera_key_speed * math.cos(r)
                 new_position[2] -= self._camera_key_speed * math.sin(r)
                 self._set_camera_position(new_position)
+                return True
             elif key == QtCore.Qt.Key_W:
                 new_position[0] += self._camera_key_speed * math.cos(r + math.pi/2)
                 new_position[2] += self._camera_key_speed * math.sin(r + math.pi/2)
                 self._set_camera_position(new_position)
+                return True
             elif key == QtCore.Qt.Key_S:
                 new_position[0] -= self._camera_key_speed * math.cos(r + math.pi/2)
                 new_position[2] -= self._camera_key_speed * math.sin(r + math.pi/2)
                 self._set_camera_position(new_position)
+                return True
 
     def following_output(self):
         return False

commit 35c8f675c4045a16d3a9a080db4c0b7ce2cce443

fixed bug introduced in 6aa2e831c1960b39c028d2d400e14089862591cc

dance-cognition/tracked_users_viewer.py
@@ -306,7 +306,8 @@ class TrackedUsersScene(Scene):
             self._drag_y_previous = y
             self.updateGL()
         else:
-            if Scene.mouseMoveEvent(self, event):
+            Scene.mouseMoveEvent(self, event)
+            if self._dragging_orientation or self._dragging_y_position:
                 self.updateGL()
 
     def mouseReleaseEvent(self, event):

dance-cognition/ui/scene.py
@@ -93,10 +93,8 @@ class Scene(QtOpenGL.QGLWidget):
             self._set_camera_orientation(
                 self._camera_y_orientation + self._camera_drag_speed * (x - self._drag_x_previous),
                 self._camera_x_orientation + self._camera_drag_speed * (y - self._drag_y_previous))
-            return True
         elif self._dragging_y_position:
             self._camera_position[1] += self._camera_y_speed * (y - self._drag_y_previous)
-            return True
         self._drag_x_previous = x
         self._drag_y_previous = y
 