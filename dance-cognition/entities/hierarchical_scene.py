312e95a114c5ec98611e3651f5a15934e62d40e5
Alex Berman <alex@nimations.com>
2015-07-13 08:40:27 +0200

    Revert "optimized new floor rendering"
    This reverts commit fa92cfa705dd38b763eecf80d195ea32534aba2b.

--- dance-cognition/entities/hierarchical_scene.py
+++ dance-cognition/entities/hierarchical_scene.py
@@ -1,14 +1,10 @@
 from ui.ui import *
 
-FLOOR_SPOT_RADIUS = 30
-FLOOR_GRID_SIZE = 5
-
 class Scene(BvhScene):
     def __init__(self, *args, **kwargs):
         BvhScene.__init__(self, *args, **kwargs)
         self._camera_translation = None
         self._camera_movement = None
-        self._floor_spot_display_list_id = None
 
     def camera_translation(self):
         if self._camera_translation is not None:
@@ -58,44 +54,38 @@ class Scene(BvhScene):
         if self._focus is not None:
             self._draw_floor_spots(
                 center_x=self._focus[0],
-                center_z=self._focus[1])
+                center_z=self._focus[1],
+                spot_radius=30, grid_size=5)
 
-    def _draw_floor_spots(self, center_x, center_z):
-        cell_size = FLOOR_SPOT_RADIUS * 2
+    def _draw_floor_spots(self, center_x, center_z, spot_radius, grid_size):
+        cell_size = spot_radius * 2
         center_nx = int(center_x / cell_size)
         center_nz = int(center_z / cell_size)
-        for nx in range(FLOOR_GRID_SIZE):
-            x = (center_nx + nx - float(FLOOR_GRID_SIZE)/2 + 0.5) * cell_size
+        for nx in range(grid_size):
+            x = (center_nx + nx - float(grid_size)/2 + 0.5) * cell_size
             if nx % 2 == 0:
                 offset_z = 0
             else:
-                offset_z = FLOOR_SPOT_RADIUS
-            for nz in range(FLOOR_GRID_SIZE):
-                z = offset_z + (center_nz + nz - float(FLOOR_GRID_SIZE)/2 + 0.5) * cell_size
-                self._draw_floor_spot(x=x, z=z)
+                offset_z = spot_radius
+            for nz in range(grid_size):
+                z = offset_z + (center_nz + nz - float(grid_size)/2 + 0.5) * cell_size
+                self._draw_floor_spot(
+                    radius=spot_radius,
+                    color=self._parent.color_scheme["floor"],
+                    x=x, z=z)
 
-    def _draw_floor_spot(self, x, z, y=0):
-        if self._floor_spot_display_list_id is None:
-            self._create_floor_spot_display_list()
-        glPushMatrix()
-        glTranslatef(x, y, z)
-        glCallList(self._floor_spot_display_list_id)
-        glPopMatrix()
+    def _draw_floor_spot(self, radius, color,
+                         x, z, y=0, resolution=15):
+        color_r, color_g, color_b, color_a = color
 
-    def _create_floor_spot_display_list(self, resolution=15):
-        floor_color = self._parent.color_scheme["floor"]
-        color_r, color_g, color_b, color_a = floor_color
         angle_increment = (float) (2 * math.pi / resolution);
-        self._floor_spot_display_list_id = glGenLists(1)
-        glNewList(self._floor_spot_display_list_id, GL_COMPILE)
         glBegin(GL_TRIANGLE_FAN)
-        glColor4f(*floor_color)
-        glVertex3f(0, 0, 0)
+        glColor4f(*color)
+        glVertex3f(x, y, z)
         glColor4f(color_r, color_g, color_b, 0)
         for i in range(resolution):
             angle1 = angle_increment * i
             angle2 = angle_increment * (i+1)
-            glVertex3f(math.cos(angle1) * FLOOR_SPOT_RADIUS, 0, math.sin(angle1) * FLOOR_SPOT_RADIUS)
-            glVertex3f(math.cos(angle2) * FLOOR_SPOT_RADIUS, 0, math.sin(angle2) * FLOOR_SPOT_RADIUS)
+            glVertex3f(x + math.cos(angle1) * radius, y, z + math.sin(angle1) * radius)
+            glVertex3f(x + math.cos(angle2) * radius, y, z + math.sin(angle2) * radius)
         glEnd()
-        glEndList()
