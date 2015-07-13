b8caaea1e1e3de988ca0fc913f4bb7b3724acbaa
Alex Berman <alex@nimations.com>
2015-07-13 08:18:06 +0200

    new floor design (only for hierarchical scene). prevents floor from disappearing when avatar moves a lot.

--- dance-cognition/entities/hierarchical_scene.py
+++ dance-cognition/entities/hierarchical_scene.py
@@ -49,3 +49,43 @@ class Scene(BvhScene):
     def central_output_position(self, output):
         root_vertex = output[0]
         return numpy.array([root_vertex[0], root_vertex[2]])
+
+    def draw_floor(self):
+        if self._focus is not None:
+            self._draw_floor_spots(
+                center_x=self._focus[0],
+                center_z=self._focus[1],
+                spot_radius=30, grid_size=5)
+
+    def _draw_floor_spots(self, center_x, center_z, spot_radius, grid_size):
+        cell_size = spot_radius * 2
+        center_nx = int(center_x / cell_size)
+        center_nz = int(center_z / cell_size)
+        for nx in range(grid_size):
+            x = (center_nx + nx - float(grid_size)/2 + 0.5) * cell_size
+            if nx % 2 == 0:
+                offset_z = 0
+            else:
+                offset_z = spot_radius
+            for nz in range(grid_size):
+                z = offset_z + (center_nz + nz - float(grid_size)/2 + 0.5) * cell_size
+                self._draw_floor_spot(
+                    radius=spot_radius,
+                    color=self._parent.color_scheme["floor"],
+                    x=x, z=z)
+
+    def _draw_floor_spot(self, radius, color,
+                         x, z, y=0, resolution=15):
+        color_r, color_g, color_b, color_a = color
+
+        angle_increment = (float) (2 * math.pi / resolution);
+        glBegin(GL_TRIANGLE_FAN)
+        glColor4f(*color)
+        glVertex3f(x, y, z)
+        glColor4f(color_r, color_g, color_b, 0)
+        for i in range(resolution):
+            angle1 = angle_increment * i
+            angle2 = angle_increment * (i+1)
+            glVertex3f(x + math.cos(angle1) * radius, y, z + math.sin(angle1) * radius)
+            glVertex3f(x + math.cos(angle2) * radius, y, z + math.sin(angle2) * radius)
+        glEnd()
