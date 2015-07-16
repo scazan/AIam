92b056b24c781388b3625de7065f634ba0bb8dd7
Alex Berman <alex@nimations.com>
2015-07-16 08:15:24 +0200

    floor grid can no longer disappear, at the cost of less appealing fade-out in the horizon

--- dance-cognition/ui/color_schemes.py
+++ dance-cognition/ui/color_schemes.py
@@ -5,7 +5,7 @@ color_schemes = OrderedDict()
 color_schemes["white"] = {
     "background": (1.0, 1.0, 1.0, 0.0),
     "unit_cube": (0, 0, 0, 0.2),
-    "floor": (0, 0, 0, 0.2),
+    "floor": (0.2, 0.2, 0.2, 1),
     "focus": (0, 0, 0, 0.2),
     "input": (0, 1, 0),
     "output": (0, 0, 0),
@@ -15,7 +15,7 @@ color_schemes["white"] = {
 color_schemes["black"] = {
     "background": (0.0, 0.0, 0.0, 0.0),
     "unit_cube": (1, 1, 1, 0.2),
-    "floor": (1, 1, 1, 0.3),
+    "floor": (0.3, 0.3, 0.3, 1),
     "focus": (1, 1, 1, 0.2),
     "input": (0, 1, 0),
     "output": (1, 1, 1),
--- dance-cognition/ui/floor_grid.py
+++ dance-cognition/ui/floor_grid.py
@@ -1,4 +1,5 @@
 from OpenGL.GL import *
+import math
 
 class FloorGrid:
     def __init__(self, num_cells, size, floor_color, background_color, y=0):
@@ -7,50 +8,58 @@ class FloorGrid:
         self._floor_color = floor_color
         self._background_color = background_color
         self._y = y
+        self._cell_size = self._size / self._num_cells
+        self._hypotenuse = math.sqrt(self._cell_size * self._cell_size * 2)
 
     def render(self, center_x, center_z, camera_x, camera_z):
-        z1 = -self._size/2
-        z2 = self._size/2
-        x1 = -self._size/2
-        x2 = self._size/2
-        color_r, color_g, color_b, color_a = self._floor_color
+        self._draw_grid(center_x, center_z)
+        self._draw_shader(-camera_x, -camera_z)
 
+    def _draw_grid(self, center_x, center_z):
+        z1 = - self._size/2
+        z2 = + self._size/2
+        x1 = - self._size/2
+        x2 = + self._size/2
+
+        glColor4f(*self._floor_color)
         glLineWidth(1.4)
 
+        quantified_center_x = int(center_x / self._hypotenuse) * self._hypotenuse
+        quantified_center_z = int(center_z / self._hypotenuse) * self._hypotenuse
         glPushMatrix()
+        glTranslatef(quantified_center_x, 0, quantified_center_z)
         glRotatef(45, 0, 1, 0)
 
         for n in range(self._num_cells):
             glBegin(GL_LINES)
-            x = x1 + float(n) / self._num_cells * self._size
-            color_a1 = color_a * (1 - abs(x - camera_x) / self._size)
-
-            glColor4f(color_r, color_g, color_b, 0)
+            x = x1 + float(n) * self._cell_size
             glVertex3f(x, self._y, z1)
-            glColor4f(color_r, color_g, color_b, color_a1)
-            glVertex3f(x, self._y, camera_z)
-
-            glColor4f(color_r, color_g, color_b, color_a1)
-            glVertex3f(x, self._y, camera_z)
-            glColor4f(color_r, color_g, color_b, 0)
             glVertex3f(x, self._y, z2)
             glEnd()
 
         for n in range(self._num_cells):
             glBegin(GL_LINES)
-            z = z1 + float(n) / self._num_cells * self._size
-            color_a1 = color_a * (1 - abs(z - camera_z) / self._size)
-
-            glColor4f(color_r, color_g, color_b, 0)
+            z = z1 + float(n) * self._cell_size
             glVertex3f(x1, self._y, z)
-            glColor4f(color_r, color_g, color_b, color_a1)
-            glVertex3f(camera_x, self._y, z)
-
-            glColor4f(color_r, color_g, color_b, color_a1)
-            glVertex3f(camera_x, self._y, z)
-            glColor4f(color_r, color_g, color_b, 0)
             glVertex3f(x2, self._y, z)
             glEnd()
 
         glPopMatrix()
 
+    def _draw_shader(self, x, z, y=0, resolution=15):
+        radius = math.sqrt(self._size * self._size * 2)
+        bg_r, bg_g, bg_b, bg_a = self._background_color
+        angle_increment = 2 * math.pi / resolution
+        glPushMatrix()
+        glTranslatef(x, 0, z)
+        glBegin(GL_TRIANGLE_FAN)
+        glColor4f(bg_r, bg_g, bg_b, 0)
+        glVertex3f(0, 0, 0)
+        glColor4f(bg_r, bg_g, bg_b, 1)
+        for i in range(resolution):
+            angle1 = angle_increment * i
+            angle2 = angle_increment * (i+1)
+            glVertex3f(math.cos(angle1) * radius, 0, math.sin(angle1) * radius)
+            glVertex3f(math.cos(angle2) * radius, 0, math.sin(angle2) * radius)
+        glEnd()
+        glPopMatrix()
