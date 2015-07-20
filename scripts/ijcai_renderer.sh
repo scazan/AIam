4fd2007596a9b834ff77e10db7da16e764b34d65
Alex Berman <alex@nimations.com>
2015-07-20 14:30:38 +0200

    one-click installation start (the added scripts can e.g. be invoked by .desktop launchers in ubuntu)

---
+++ scripts/ijcai_renderer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#INTERPRETER_VIEWER_ARGS=--with-viewer
+
+cd ~/projects/AIam/dance-cognition
+python dim_reduce.py -p valencia_quaternion_7d_friction --mode=improvise --color-scheme=black --no-toolbar --floor-renderer=checkerboard --preferred-location=0.0783783783784,0.315135135135,0.590810810811,0.240540540541,0.392972972973,0.448108108108,0.626486486486 --websockets --launch-when-ready="gnome-terminal -e 'python interpret_user_movement.py $INTERPRETER_VIEWER_ARGS --tracker=0,-7 --active-area-center=0,3300'"
+
+# force terminal window to stay open
+bash
---
+++ scripts/ijcai_tracker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~/projects/AIam/tracking
+./Bin/x64-Release/Tracker -smooth 0.6
+
+# force terminal window to stay open
+bash
