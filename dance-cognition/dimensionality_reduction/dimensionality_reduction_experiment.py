scazan/AIam

commit 01af60702efca9fec8cd0bf9973c42d4a901ad09

Optimized startup significantly: BVH frames only loaded when training or with --mode=follow. Also, --mode=follow is now required for enabling this mode.

dance-cognition/dimensionality_reduction/dimensionality_reduction_experiment.py
@@ -24,7 +24,7 @@ class DimensionalityReductionExperiment(Experiment):
                             choices=[modes.FOLLOW,
                                      modes.IMPROVISE,
                                      modes.EXPLORE],
-                            default=modes.FOLLOW)
+                            default=modes.EXPLORE)
         parser.add_argument("--max-novelty", type=float, default=1.)
         parser.add_argument("--plot-velocity")
         parser.add_argument("--analyze-components", action="store_true")
@@ -356,6 +356,9 @@ class DimensionalityReductionExperiment(Experiment):
             output, self._pose_for_feature_extraction)
         features = self.entity.extract_features(self._pose_for_feature_extraction)
         return features
+
+    def should_read_bvh_frames(self):
+        return self.args.train or self.args.mode == modes.FOLLOW
 
 class ImproviserParameters(Parameters):
     def __init__(self):

dance-cognition/bvh/bvh_collection.py
@@ -14,17 +14,17 @@ class BvhCollection:
     def get_readers(self):
         return self._readers
 
-    def read(self):
-        self._read_bvhs()
+    def read(self, read_frames=True):
+        self._read_bvhs(read_frames)
         self._set_scale_info()
         self._hierarchy = self._create_hierachy()
 
-    def _read_bvhs(self):
+    def _read_bvhs(self, read_frames):
         self._duration = 0
         frame_offset = 0
         index = 0
         for reader in self._readers:
-            reader.read()
+            reader.read(read_frames)
             reader.index = index
             reader.start_time = self._duration
             reader.end_time = self._duration + reader.get_duration()

dance-cognition/bvh/bvh_reader.py
@@ -18,9 +18,9 @@ CHANNEL_TO_AXIS = {
 }
 
 class BvhReader(cgkit.bvh.BVHReader):
-    def read(self):
+    def read(self, read_frames=True):
         if self._cache_exists():
-            self._read()
+            self._read(read_frames)
             self._load_from_cache()
         else:
             self._read()
@@ -54,8 +54,8 @@ class BvhReader(cgkit.bvh.BVHReader):
     def _cache_filename(self):
         return "%s.cache" % self.filename
 
-    def _read(self):
-        cgkit.bvh.BVHReader.read(self)
+    def _read(self, read_frames):
+        cgkit.bvh.BVHReader.read(self, read_frames)
         self.hierarchy = self._create_hierarchy()
         self.num_joints = self.hierarchy.num_joints
         self._duration = self._num_frames * self._frame_time

dance-cognition/dimensionality_reduction/ui/dimensionality_reduction_ui.py
@@ -137,6 +137,7 @@ class DimensionalityReductionToolbar(ExperimentToolbar):
             self._add_features_tab_widgets()
         self.setLayout(self._layout)
         self.set_mode(self.args.mode)
+        self._activate_current_mode()
 
     def set_mode(self, mode):
         mode_tab = self._mode_tabs[mode]
@@ -147,7 +148,8 @@ class DimensionalityReductionToolbar(ExperimentToolbar):
     def _add_mode_tabs(self):
         self.tabs = QtGui.QTabWidget()
         self._mode_tabs = {}
-        self._add_follow_tab()
+        if self.args.mode == modes.FOLLOW:
+            self._add_follow_tab()
         self._add_explore_tab()
         self._add_improvise_tab()
         self.tabs.currentChanged.connect(self._changed_mode_tab)
@@ -155,13 +157,16 @@ class DimensionalityReductionToolbar(ExperimentToolbar):
         self._changing_mode_non_interactively = False
 
     def _changed_mode_tab(self):
+        self._activate_current_mode()
+        if not self._changing_mode_non_interactively:
+            self.parent().send_event(
+                Event(Event.MODE, self.tabs.currentWidget()._mode_id))
+
+    def _activate_current_mode(self):
         exploring = (self.tabs.currentWidget() == self.explore_tab)
         for n in range(self._reduction_tabs.count()):
             tab = self._reduction_tabs.widget(n)
             tab.set_enabled(exploring)
-        if not self._changing_mode_non_interactively:
-            self.parent().send_event(
-                Event(Event.MODE, self.tabs.currentWidget()._mode_id))
 
     def _add_follow_tab(self):
         self.follow_tab = ModeTab(modes.FOLLOW)

dance-cognition/experiment.py
@@ -143,7 +143,7 @@ class Experiment(EventListener):
                 raise Exception("no files found matching the pattern %s" % args.bvh)
             print "loading BVHs from %s..." % args.bvh
             self.bvh_reader = BvhCollection(bvh_filenames)
-            self.bvh_reader.read()
+            self.bvh_reader.read(read_frames=self.should_read_bvh_frames())
             print "ok"
             self.bvh_writer = BvhWriter(self.bvh_reader.get_hierarchy(), self.bvh_reader.get_frame_time())
             self.pose = self.bvh_reader.get_hierarchy().create_pose()

dance-cognition/libs/cgkit/bvh.py
@@ -31,6 +31,9 @@
 # the provisions above, a recipient may use your version of this file under
 # the terms of any one of the MPL, the GPL or the LGPL.
 #
+# History:
+# Alex Berman: added read_frames argument to the read method
+#
 # ***** END LICENSE BLOCK *****
 # $Id: bvh.py,v 1.1 2005/02/06 22:26:02 mbaas Exp $
 
@@ -85,17 +88,17 @@ class BVHReader:
         pass
 
     # read
-    def read(self):
+    def read(self, read_frames=True):
         """Read the entire file.
         """
         self.fhandle = file(self.filename)
 
         self.readHierarchy()
         self.onHierarchy(self._root)
-        self.readMotion()
+        self.readMotion(read_frames)
 
     # readMotion
-    def readMotion(self):
+    def readMotion(self, read_frames):
         """Read the motion samples.
         """
         # No more tokens (i.e. end of file)? Then just return 
@@ -126,15 +129,15 @@ class BVHReader:
 
         self.onMotion(frames, dt)
 
-        # Read the channel values
-        for i in range(frames):
-            s = self.readLine()
-            a = s.split()
-            if len(a)!=self._numchannels:
-                raise SyntaxError("Syntax error in line %d: %d float values expected, got %d instead"%(self.linenr, self._numchannels, len(a)))
-            values = map(lambda x: float(x), a)
-            self.onFrame(values)
-
+        if read_frames:
+            # Read the channel values
+            for i in range(frames):
+                s = self.readLine()
+                a = s.split()
+                if len(a)!=self._numchannels:
+                    raise SyntaxError("Syntax error in line %d: %d float values expected, got %d instead"%(self.linenr, self._numchannels, len(a)))
+                values = map(lambda x: float(x), a)
+                self.onFrame(values)
 
     # readHierarchy
     def readHierarchy(self):

dance-cognition/prediction_experiment.py
@@ -74,3 +74,6 @@ class PredictionExperiment(Experiment):
             self.teacher.proceed(time_increment)
             t += time_increment
         print "ok"
+
+    def should_read_bvh_frames(self):
+        return True

commit 61c0a3ec5b8831fa9c717bbd4e42195105e0bcf0

Solved bug introduced by 01af60702efca9fec8cd0bf9973c42d4a901ad09

dance-cognition/dimensionality_reduction/dimensionality_reduction_experiment.py
@@ -418,14 +418,10 @@ class Improviser:
             if self.experiment.args.preferred_location:
                 return self.experiment.preferred_location
             else:
-                unnormalized_departure = self._get_stimulus()
+                unnormalized_departure = numpy.array([.5] * self.experiment.args.num_components)
         else:
             unnormalized_departure = self.experiment.reduction
         return self.experiment.student.normalize_reduction(unnormalized_departure)
-
-    def _get_stimulus(self):
-        return self.experiment.student.transform(numpy.array([
-                    self.experiment.get_adapted_stimulus_value()]))[0]
 
     def _interpolate_path(self, path_segments):
         return interpolation.interpolate(