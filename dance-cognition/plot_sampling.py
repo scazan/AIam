0ce5eff993e8695c5860f6dc4aac71b30c2d9aba
Alex Berman <alex@nimations.com>
2016-02-26 11:00:01 +0100

    plot_sampling.py supports --plot-dimensios

--- dance-cognition/plot_sampling.py
+++ dance-cognition/plot_sampling.py
@@ -2,6 +2,7 @@ from dimensionality_reduction.dimensionality_reduction_experiment import *
 
 parser = ArgumentParser()
 parser.add_argument("--output", "-o", default="samples.dat")
+parser.add_argument("--plot-dimensions", help="e.g. 0,3 (x as 1st dimension and y as 4th)")
 
 DimensionalityReductionExperiment.add_parser_arguments(parser)
 experiment = DimensionalityReductionExperiment(parser)
@@ -14,7 +15,13 @@ samples = sampling.NeighborhoodSampler.sample(
     samples_per_neighborhood=10,
     neighborhood_size=0.1)
 
-out = open(experiment.args.output, "w")
+args = experiment.args
+if args.plot_dimensions:
+    dimensions = [int(string) for string in args.plot_dimensions.split(",")]
+else:
+    dimensions = [0, 1]
+
+out = open(args.output, "w")
 for sample in samples:
-    print >>out, " ".join([str(value) for value in sample])
+    print >>out, " ".join([str(sample[n]) for n in dimensions])
 out.close()
