4ece85596289bfe9862045029c26342d15de8ba4
Alex Berman <alex@nimations.com>
2016-02-27 07:56:47 +0100

    plot_sampling.py deals with dimension selection properly

--- dance-cognition/plot_sampling.py
+++ dance-cognition/plot_sampling.py
@@ -9,26 +9,25 @@ from dimensionality_reduction.dimensionality_reduction_experiment import *
 
 parser = ArgumentParser()
 parser.add_argument("--output", "-o", default="samples.dat")
-parser.add_argument("--plot-dimensions", help="e.g. 0,3 (x as 1st dimension and y as 4th)")
+parser.add_argument("--select-dimensions",
+                    default="0,1",
+                    help="e.g. 0,3 (x as 1st dimension and y as 4th)")
 
 DimensionalityReductionExperiment.add_parser_arguments(parser)
 experiment = DimensionalityReductionExperiment(parser)
 experiment._load_model()
 
-data = experiment.student.normalized_observed_reductions
+args = experiment.args
+selected_dimensions = [int(string) for string in args.select_dimensions.split(",")]
+
+data = experiment.student.normalized_observed_reductions[:,selected_dimensions]
 samples = sampling.NeighborhoodSampler.sample(
     data,
     num_neighborhoods=100,
     samples_per_neighborhood=10,
     neighborhood_size=0.1)
 
-args = experiment.args
-if args.plot_dimensions:
-    dimensions = [int(string) for string in args.plot_dimensions.split(",")]
-else:
-    dimensions = [0, 1]
-
 out = open(args.output, "w")
 for sample in samples:
-    print >>out, " ".join([str(sample[n]) for n in dimensions])
+    print >>out, " ".join([str(value) for value in sample])
 out.close()
