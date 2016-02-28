253d2dc653b171f55266d9a8248e2b9deb0db95e
Alex Berman <alex@nimations.com>
2016-02-28 08:50:27 +0100

    Added DistanceDistributionEqualizationSampler

--- dance-cognition/sampling.py
+++ dance-cognition/sampling.py
@@ -1,5 +1,6 @@
 import numpy
 import sklearn.cluster
+import sklearn.neighbors
 
 class NeighborhoodSampler:
     @classmethod
@@ -35,3 +36,25 @@ class MiniBatchKMeansSampler:
         kmeans = sklearn.cluster.MiniBatchKMeans(n_clusters=num_samples)
         kmeans.fit(data)
         return kmeans.cluster_centers_
+
+class DistanceDistributionEqualizationSampler:
+    def sample(self, data, num_samples):
+        self._data = data
+        self._knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=2)
+        indices = range(len(data))
+        self._knn.fit(data, indices)
+        indices_sorted_by_nearest_neighbor_distance = sorted(
+            indices,
+            key=lambda index: self._distance_to_nearest_neighbor(index))
+        linear_index_distribution = [
+            int(n) for n in numpy.arange(0, len(data), float(len(data))/num_samples)]
+        sampled_indices = [
+            indices_sorted_by_nearest_neighbor_distance[index]
+            for index in linear_index_distribution]
+        return [data[index] for index in sampled_indices]
+
+    def _distance_to_nearest_neighbor(self, index):
+        observation = self._data[index]
+        distances_list, _ = self._knn.kneighbors(observation, return_distance=True)
+        distances = distances_list[0]
+        return max(distances)
