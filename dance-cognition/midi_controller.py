23d982b943f97463ed51e122cd1adfa5a915bfaa
Alex Berman <alex@nimations.com>
2016-04-09 17:00:20 +0200

    midi_controller supports float ranges beyond 0-1

--- dance-cognition/midi_controller.py
+++ dance-cognition/midi_controller.py
@@ -9,19 +9,20 @@ import time
 from websocket_client import WebsocketClient
 from event_listener import EventListener
 from event import Event
+from dimensionality_reduction.behaviors.hybrid import HybridParameters
+from parameters import *
 
 WEBSOCKET_HOST = "localhost"
-PARAMETERS = [
-    "imitation",
-    "flaneur_translational_speed"]
+PARAMETERS = HybridParameters()
 
 class MidiController:
     @staticmethod
     def add_parser_arguments(parser):
         parser.add_argument("--port")
         parser.add_argument("--calibrate", action="store_true")
-        for parameter_name in PARAMETERS:
-            parser.add_argument("--%s" % parameter_name, help="command,channel")
+        for parameter in PARAMETERS:
+            if parameter.choices.__class__ is ParameterFloatRange:
+                parser.add_argument("--%s" % parameter.name, help="command,channel")
 
     def __init__(self, args):
         self._args = args
@@ -31,10 +32,10 @@ class MidiController:
 
     def _create_parameter_lookup_table(self):
         self._parameter_lookup_table = {}
-        for parameter_name in PARAMETERS:
-            command_comma_channel = getattr(self._args, parameter_name)
+        for parameter in PARAMETERS:
+            command_comma_channel = getattr(self._args, parameter.name)
             if command_comma_channel:
-                self._parameter_lookup_table[command_comma_channel] = parameter_name
+                self._parameter_lookup_table[command_comma_channel] = parameter
 
     def _callback(self, event, data=None):
         message, deltatime = event
@@ -43,16 +44,17 @@ class MidiController:
         if self._args.calibrate:
             print command_comma_channel
         if command_comma_channel in self._parameter_lookup_table:
-            parameter_name = self._parameter_lookup_table[command_comma_channel]
-            self._process_value(parameter_name, value)
+            parameter = self._parameter_lookup_table[command_comma_channel]
+            self._process_value(parameter, value)
 
-    def _process_value(self, parameter_name, midi_value):
-        float_value = float(midi_value) / 127
+    def _process_value(self, parameter, midi_value):
+        parameter_value = float(midi_value) / 127 * parameter.choices.range + \
+            parameter.choices.min_value
         event_sender.send_event(
             Event(Event.PARAMETER,
                   {"class": "HybridParameters",
-                   "name": parameter_name,
-                   "value": float_value}))
+                   "name": parameter.name,
+                   "value": parameter_value}))
                 
     def main_loop(self):
         print "press ctrl-c to exit"
