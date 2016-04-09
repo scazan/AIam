5ea4e3679b31ff328b33471ce6d018c23262239b
Alex Berman <alex@nimations.com>
2016-04-09 16:59:45 +0200

    Increased max flaneur translational speed

--- dance-cognition/dimensionality_reduction/behaviors/flaneur_behavior.py
+++ dance-cognition/dimensionality_reduction/behaviors/flaneur_behavior.py
@@ -6,7 +6,7 @@ class FlaneurParameters(Parameters):
     def __init__(self):
         Parameters.__init__(self)
         self.add_parameter("translational_speed", type=float, default=.6,
-                           choices=ParameterFloatRange(0., 1.))
+                           choices=ParameterFloatRange(0., 3.))
         self.add_parameter("directional_speed", type=float, default=1.1,
                            choices=ParameterFloatRange(0., 3.))
         self.add_parameter("look_ahead_distance", type=float, default=.2,
