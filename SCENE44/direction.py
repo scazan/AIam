7e02b8ddd02ee70d26ef5d408df1272ee3e7a7cc
Gáspár Hajdu <gase12@gmail.com>
2016-11-16 18:07:13 +0100

    final sketch for day 2 - send dancer to target position by voice commands

--- SCENE44/direction.py
+++ SCENE44/direction.py
@@ -1,6 +1,6 @@
 import os
 import random
-from math import atan2, degrees, pi
+from math import atan2, degrees, pi, sqrt
 # import numpy as np
 import time
 import sys
@@ -12,28 +12,48 @@ from osc_receiver import OscReceiver
 OSC_PORT = 15002
 
 
+targets = [[132,4500],[1500,3000],[-1500,3000],[-1500,6000],[1500,6000]]
+
+target = targets[0]
+print target
+target_reached = False
+
 d = {
-	0: 'East',
-	1: 'Northeast',
-	2: 'North',
-	3: 'Northwest',
-	4: 'West',
-	5: 'Southwest ',
-	6: 'South',
-	7: 'Southeast',
-	8: 'East'
+	0: 'Windows',
+	1: 'Windows Office',
+	2: 'Office',
+	3: 'Office Bathroom',
+	4: 'Bathroom',
+	5: 'Bathroom Screen',
+	6: 'Screen',
+	7: 'Screen Windows',
+	8: 'Windows'
 }
 
+def distance(p0, p1):
+    return sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
 def handle_center(path, values, types, src, user_data):
 
 	global time_of_last_handler
-	if time_of_last_handler is None or time.time() - time_of_last_handler > 4:
-		user_id, x, y, z = values
-		print user_id, x, y, z
-		getDir(x,y,0,0)
-		time_of_last_handler = time.time()
+	global target
+	# check distance from current target
+	user_id, x, y, z = values
+	d = distance([x,z], target)
+	
+	if d<400 :
+		# os.system("say Target reached")
+		# os.system("say new target")
 
+		target_reached = False
+		#generate new target
+		target=random.choice(targets)
+		print "target:", target
+	elif time_of_last_handler is None or time.time() - time_of_last_handler > 1 :
+		print user_id, x, y, z
+		getDir(x,z,target[0],target[1])
+		time_of_last_handler = time.time()
+		print "distance:", d
 
 time_of_last_handler = None
 osc_receiver = OscReceiver(OSC_PORT)
@@ -58,11 +78,11 @@ def getDir(x1,y1,x2,y2):
 	rads %= 2*pi
 	degs = degrees(rads)
 
-	directionId = round(350/45.0)
-	print(degs)
+	directionId = round(degs/45.0)
+	# print(degs)
 
 	direction = d[directionId]
-	print(direction)
+	# print(direction)
 	os.system("say "+direction)
 
 
