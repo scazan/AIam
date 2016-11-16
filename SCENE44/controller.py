42b914b429e5dd3b24889f226acfb253d7de2ecf
Alex Berman <alex@nimations.com>
2016-11-16 15:33:55 +0100

    Added SCENE44 folder

---
+++ SCENE44/controller.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/../dance-cognition")
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/../dance-cognition/connectivity")
+from osc_receiver import OscReceiver
