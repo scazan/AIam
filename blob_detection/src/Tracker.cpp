scazan/AIam

commit c0186908944cb853265ee6da7636089287180434

all processing methods now receive a ref to an opencv Mat (8-bit uchars) instead of an openni pixel array (millimeter integers)

blob_detection/src/Tracker.cpp
@@ -143,12 +143,28 @@ void Tracker::mainLoop() {
 	glutMainLoop();
 }
 
-openni::VideoFrameRef Tracker::getDepthFrame() {
-	openni::Status status = depthStream.readFrame(&depthFrame);
-	if (status != openni::STATUS_OK) {
-		printf("Couldn't read depth frame:\n%s\n", openni::OpenNI::getExtendedError());
-	}
-	return depthFrame;
+void Tracker::processOniDepthFrame() {
+  if (depthFrame.empty())
+    depthFrame.create(height, width, CV_8UC1);
+
+  const openni::DepthPixel* pOniRow =
+      (const openni::DepthPixel*) oniDepthFrame.getData();
+  int rowSize = oniDepthFrame.getStrideInBytes() / sizeof(openni::DepthPixel);
+  uchar depth;
+  uchar *matPtr;
+
+  for (int y = 0; y < height; ++y) {
+    const openni::DepthPixel* pOni = pOniRow;
+    matPtr = depthFrame.ptr(y);
+    for (int x = 0; x < width; ++x, ++pOni) {
+      if (*pOni != 0 && *pOni < depthThreshold)
+        depth = (int) (255 * (1 - float(*pOni) / depthThreshold));
+      else
+        depth = 0;
+      *matPtr++ = depth;
+    }
+    pOniRow += rowSize;
+  }
 }
 
 void Tracker::onWindowResized(int width, int height)
@@ -170,9 +186,15 @@ void Tracker::display()
 	}
 	previousDisplayTime = currentDisplayTime;
 
-	depthFrame = getDepthFrame();
-	if(!depthFrame.isValid())
+  openni::Status status = depthStream.readFrame(&oniDepthFrame);
+  if (status != openni::STATUS_OK) {
+    printf("Couldn't read depth frame:\n%s\n", openni::OpenNI::getExtendedError());
+  }
+
+	if(!oniDepthFrame.isValid())
 		return;
+
+	processOniDepthFrame();
 
 	if(processingEnabled)
 		processingMethod->processDepthFrame(depthFrame);
@@ -208,9 +230,9 @@ void Tracker::display()
 }
 
 void Tracker::calculateHistogram() {
-	const openni::DepthPixel* pDepth = (const openni::DepthPixel*)depthFrame.getData();
+	const openni::DepthPixel* pDepth = (const openni::DepthPixel*)oniDepthFrame.getData();
 	memset(histogram, 0, MAX_DEPTH * sizeof(float));
-	int restOfRow = depthFrame.getStrideInBytes() / sizeof(openni::DepthPixel) - width;
+	int restOfRow = oniDepthFrame.getStrideInBytes() / sizeof(openni::DepthPixel) - width;
 
 	unsigned int nNumberOfPoints = 0;
 	for (int y = 0; y < height; ++y)
@@ -241,16 +263,16 @@ void Tracker::calculateHistogram() {
 void Tracker::updateTextureMap() {
 	memset(textureMap, 0, textureMapWidth*textureMapHeight*sizeof(openni::RGB888Pixel));
 
-	const openni::DepthPixel* pDepthRow = (const openni::DepthPixel*)depthFrame.getData();
-	openni::RGB888Pixel* textureMapRow = textureMap + depthFrame.getCropOriginY() * textureMapWidth;
-	int rowSize = depthFrame.getStrideInBytes() / sizeof(openni::DepthPixel);
+	const openni::DepthPixel* pDepthRow = (const openni::DepthPixel*)oniDepthFrame.getData();
+	openni::RGB888Pixel* textureMapRow = textureMap + oniDepthFrame.getCropOriginY() * textureMapWidth;
+	int rowSize = oniDepthFrame.getStrideInBytes() / sizeof(openni::DepthPixel);
 	unsigned char depth_uchar;
 
 	log("rowSize=%d height=%d width=%d\n", rowSize, height, width);
 	for (int y = 0; y < height; ++y)
 	{
 		const openni::DepthPixel* pDepth = pDepthRow;
-		openni::RGB888Pixel* pTex = textureMapRow + depthFrame.getCropOriginX();
+		openni::RGB888Pixel* pTex = textureMapRow + oniDepthFrame.getCropOriginX();
 
 		for (int x = 0; x < width; ++x, ++pDepth, ++pTex)
 		{

blob_detection/src/Boids.hpp
@@ -15,24 +15,19 @@ public:
 		}
 	}
 
-	void processDepthFrame(openni::VideoFrameRef depthFrame) {
-	  const openni::DepthPixel* pOniRow;
-	  int rowSize = depthFrame.getStrideInBytes() / sizeof(openni::DepthPixel);
-	  openni::DepthPixel minDepth = MAX_DEPTH;
-	  openni::DepthPixel maxDepth = 0;
-	  openni::DepthPixel depth;
+	void processDepthFrame(Mat& frame) {
+	  uchar minDepth = 255;
+	  uchar maxDepth = 0;
+    uchar *matPtr;
+	  uchar depth;
 
-    pOniRow = (const openni::DepthPixel*) depthFrame.getData();
 	  for (int y = 0; y < height; ++y) {
-	    const openni::DepthPixel* pOni = pOniRow;
-	    for (int x = 0; x < width; ++x, ++pOni) {
-	      if (*pOni != 0) {
-	        depth = *pOni;
-	        minDepth = MIN(minDepth, depth);
-	        maxDepth = MAX(maxDepth, depth);
-	      }
+	    matPtr = frame.ptr(y);
+	    for (int x = 0; x < width; ++x, ++matPtr) {
+	      depth = *matPtr;
+        minDepth = MIN(minDepth, depth);
+        maxDepth = MAX(maxDepth, depth);
 	    }
-	    pOniRow += rowSize;
 	  }
 
 	  float relativeDepth;
@@ -42,22 +37,18 @@ public:
 		  boid->fx = 0;
 		  boid->fy = 0;
 
-      pOniRow = (const openni::DepthPixel*) depthFrame.getData();
 	    for (int py = 0; py < height; ++py) {
 	      y = (float)py / height;
-	      const openni::DepthPixel* pOni = pOniRow;
-	      for (int px = 0; px < width; ++px, ++pOni) {
+	      matPtr = frame.ptr(py);
+	      for (int px = 0; px < width; ++px, ++matPtr) {
 	        x = (float)px / width;
-	        if (*pOni != 0) {
-	          depth = *pOni;
-	          relativeDepth = ((float)depth - minDepth) / (maxDepth - minDepth);
-	          if(relativeDepth < 0.0001)
-	            relativeDepth = 0.0001;
-	          boid->fx += (x - boid->x) / relativeDepth;
-            boid->fy += (y - boid->y) / relativeDepth;
-	        }
+          depth = *matPtr;
+          relativeDepth = ((float) depth - minDepth) / (maxDepth - minDepth);
+          if (relativeDepth < 0.0001)
+            relativeDepth = 0.0001;
+          boid->fx += (x - boid->x) / relativeDepth;
+          boid->fy += (y - boid->y) / relativeDepth;
 	      }
-	      pOniRow += rowSize;
 	    }
 
 	    norm = sqrt(boid->fx*boid->fx + boid->fy*boid->fy);

blob_detection/src/DenseOpticalFlow.cpp
@@ -25,28 +25,8 @@ void DenseOpticalFlow::onKey(unsigned char key) {
   }
 }
 
-void DenseOpticalFlow::processDepthFrame(openni::VideoFrameRef depthFrame) {
-  if (frame.empty())
-    frame.create(height, width, CV_8UC1);
-
-  const openni::DepthPixel* pOniRow =
-      (const openni::DepthPixel*) depthFrame.getData();
-  int rowSize = depthFrame.getStrideInBytes() / sizeof(openni::DepthPixel);
-  uchar depth;
-  uchar *pCv;
-
-  for (int y = 0; y < height; ++y) {
-    const openni::DepthPixel* pOni = pOniRow;
-    pCv = frame.ptr(y);
-    for (int x = 0; x < width; ++x, ++pOni) {
-      if (*pOni != 0 && *pOni < depthThreshold)
-        depth = (int) (255 * (1 - float(*pOni) / depthThreshold));
-      else
-        depth = 0;
-      *pCv++ = depth;
-    }
-    pOniRow += rowSize;
-  }
+void DenseOpticalFlow::processDepthFrame(Mat& newFrame) {
+  newFrame.copyTo(frame);
 
   if (!previousFrame.empty()) {
     calcOpticalFlowFarneback(previousFrame, frame, flow, 0.5, 3, 15, 3, 5, 1.2,

blob_detection/src/DenseOpticalFlow.hpp
@@ -9,7 +9,7 @@ using namespace cv;
 class DenseOpticalFlow : public ProcessingMethod {
 public:
   DenseOpticalFlow(int width, int height, int depthThreshold);
-  void processDepthFrame(openni::VideoFrameRef);
+  void processDepthFrame(Mat&);
   void render();
   void onKey(unsigned char key);
 

blob_detection/src/LucasKanade.cpp
@@ -19,27 +19,8 @@ void LucasKanadeOpticalFlow::onKey(unsigned char key) {
   }
 }
 
-void LucasKanadeOpticalFlow::processDepthFrame(openni::VideoFrameRef depthFrame) {
-  if(frame.empty())
-    frame.create(height, width, CV_8UC1);
-
-  const openni::DepthPixel* pOniRow = (const openni::DepthPixel*)depthFrame.getData();
-  int rowSize = depthFrame.getStrideInBytes() / sizeof(openni::DepthPixel);
-  uchar depth;
-  uchar *pCv;
-
-  for (int y = 0; y < height; ++y) {
-    const openni::DepthPixel* pOni = pOniRow;
-    pCv = frame.ptr(y);
-    for (int x = 0; x < width; ++x, ++pOni) {
-      if (*pOni != 0 && *pOni < depthThreshold)
-	depth = (int) (255 * (1 - float(*pOni) / depthThreshold));
-      else
-	depth = 0;
-      *pCv++ = depth;
-    }
-    pOniRow += rowSize;
-  }
+void LucasKanadeOpticalFlow::processDepthFrame(Mat& newFrame) {
+  newFrame.copyTo(frame);
 
   const int MAX_COUNT = 500;
   TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 20, 0.03); // only once?

blob_detection/src/LucasKanade.hpp
@@ -10,7 +10,7 @@ using namespace cv;
 class LucasKanadeOpticalFlow : public ProcessingMethod {
 public:
   LucasKanadeOpticalFlow(int width, int height, int depthThreshold);
-  void processDepthFrame(openni::VideoFrameRef);
+  void processDepthFrame(Mat&);
   void render();
   void onKey(unsigned char key);
 

blob_detection/src/Tracker.hpp
@@ -2,8 +2,11 @@
 #define _TRACKER_HPP_
 
 #include "OpenNI.h"
+#include <opencv2/imgproc/imgproc.hpp>
 
 #define MAX_DEPTH 10000
+
+using namespace cv;
 
 class ProcessingMethod {
 public:
@@ -13,7 +16,7 @@ public:
     this->depthThreshold = depthThreshold;
   }
   virtual ~ProcessingMethod() {}
-  virtual void processDepthFrame(openni::VideoFrameRef)=0;
+  virtual void processDepthFrame(Mat&)=0;
   virtual void render()=0;
   virtual void onKey(unsigned char key)=0;
 
@@ -31,7 +34,7 @@ public:
   void mainLoop();
 
 private:
-  openni::VideoFrameRef getDepthFrame();
+  void processOniDepthFrame();
   void display();
   void onWindowResized(int width, int height);
   void onKey(unsigned char key);
@@ -50,7 +53,8 @@ private:
   static Tracker* self;
   openni::Device device;
   openni::VideoStream depthStream;
-  openni::VideoFrameRef depthFrame;
+  openni::VideoFrameRef oniDepthFrame;
+  Mat depthFrame;
   int width, height;
   bool depthAsPoints;
   openni::RGB888Pixel* textureMap;

commit cabfd372a907acccad303b42f6d17d566eb5aad6

fix bug introduced in c0186908944cb853265ee6da7636089287180434

blob_detection/src/Tracker.cpp
@@ -159,7 +159,7 @@ void Tracker::mainLoop() {
 
 void Tracker::processOniDepthFrame() {
   if (depthFrame.empty())
-    depthFrame.create(resolutionX, resolutionY, CV_8UC1);
+    depthFrame.create(resolutionY, resolutionX, CV_8UC1);
 
   const openni::DepthPixel* oniData =
       (const openni::DepthPixel*) oniDepthFrame.getData();