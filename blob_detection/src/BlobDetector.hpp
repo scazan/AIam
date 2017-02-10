de4ba0aea01f748a856ac8020d786ab5db52e20e
Alex Berman <alex@nimations.com>
2017-02-10 17:05:35 +0100

    WIP: extracting blog pixels: trying to use fillConvexPoly

--- blob_detection/src/BlobDetector.hpp
+++ blob_detection/src/BlobDetector.hpp
@@ -35,7 +35,28 @@ public:
     Mat binaryImage;
     threshold(depthFrame, binaryImage, 1, 255, THRESH_BINARY);
     findContours(binaryImage, contours, RETR_LIST, CHAIN_APPROX_NONE);
+    blobImages.clear();
+    for(vector< vector<Point> >::iterator i = contours.begin(); i != contours.end(); i++)
+      processContour(*i);
     detector->detect(depthFrame, keyPoints);
+  }
+
+  void processContour(const vector<Point>& contour) {
+    Mat blobImage;
+    blobImage.create(height, width, CV_8UC1);
+    // for(vector<Point>::const_iterator i = contour.begin(); i != contour.end(); i++) {
+    //   blobImage.at<unsigned int>(i->y, i->x) = 1;
+    // }
+    
+    Point *points = new Point [contour.size()];
+    int n = 0;
+    for(vector<Point>::const_iterator i = contour.begin(); i != contour.end(); i++) {
+      points[n] = Point(i->y, i->x);
+      n++;
+    }
+    fillConvexPoly(blobImage, points, contour.size(), Scalar(1,1,1));
+    delete [] points;
+    blobImages.push_back(blobImage);
   }
 
   void render() {
@@ -47,6 +68,11 @@ public:
     glColor3f(0,0,1);
     for(vector< vector<Point> >::iterator i = contours.begin(); i != contours.end(); i++) {
       drawContour(*i);
+    }
+
+    glColor3f(1,0,0);
+    for(vector<Mat>::iterator i = blobImages.begin(); i != blobImages.end(); i++) {
+      drawBlobImage(*i);
     }
   }
 
@@ -63,10 +89,22 @@ public:
     glEnd();
   }
 
-  void drawContour(const vector<Point> contour) {
+  void drawContour(const vector<Point>& contour) {
     glBegin(GL_LINE_LOOP);
     for(vector<Point>::const_iterator i = contour.begin(); i != contour.end(); i++) {
       glVertex2f((float)i->x / width, (float)i->y / height);
+    }
+    glEnd();
+  }
+
+  void drawBlobImage(Mat image) {
+    glBegin(GL_POINTS);
+    for(int y = 0; y < height; y++) {
+      for(int x = 0; x < width; x++) {
+	if(image.at<unsigned int>(y, x)) {
+	  glVertex2f((float)x / width, (float)y / height);
+	}
+      }
     }
     glEnd();
   }
@@ -77,4 +115,5 @@ private:
   SimpleBlobDetector *detector;
   std::vector<KeyPoint> keyPoints;
   std::vector < std::vector<Point> > contours;
+  vector<Mat> blobImages;
 };
