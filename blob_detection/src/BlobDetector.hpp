b4a5ad9ef5cd4baabb66a194d0e6caa473d5a1a5
Alex Berman <alex@nimations.com>
2017-02-11 11:25:18 +0100

    filtering blobs by area and experimenting with orientation estimation

--- blob_detection/src/BlobDetector.hpp
+++ blob_detection/src/BlobDetector.hpp
@@ -8,48 +8,94 @@ using namespace std;
 #include <GL/glut.h>
 #endif
 
-class BlobDetector : public ProcessingMethod {
+#define MIN_BLOB_AREA 50000
+#define MAX_BLOB_AREA 300000
+
+class BlobDetector: public ProcessingMethod {
 public:
-  BlobDetector(Tracker *tracker) : ProcessingMethod(tracker) {
+  BlobDetector(Tracker *tracker) :
+      ProcessingMethod(tracker) {
+    float worldWidth = tracker->getWorldRange().xMax - tracker->getWorldRange().xMin;
+    float worldHeight = tracker->getWorldRange().yMax - tracker->getWorldRange().yMin;
+    worldArea = worldWidth * worldHeight;
+    resolutionArea = width * height;
   }
 
   void processDepthFrame(Mat& depthFrame) {
+    std::vector<std::vector<Point> > unfilteredContours;
     Mat binaryImage;
     threshold(depthFrame, binaryImage, 1, 255, THRESH_BINARY);
-    findContours(binaryImage, contours, RETR_LIST, CHAIN_APPROX_NONE);
+    findContours(binaryImage, unfilteredContours, RETR_LIST, CHAIN_APPROX_NONE);
+    filterContours(unfilteredContours);
+
     blobImages.clear();
-    for(vector< vector<Point> >::iterator i = contours.begin(); i != contours.end(); i++)
+    for (vector<vector<Point> >::iterator i = contours.begin();
+        i != contours.end(); i++) {
       processContour(*i);
+    }
+  }
+
+  void filterContours(std::vector<std::vector<Point> > &unfilteredContours) {
+    contours.clear();
+    for (vector<vector<Point> >::iterator i = unfilteredContours.begin();
+        i != unfilteredContours.end(); i++) {
+      float pixelArea = contourArea(*i);
+      float area = pixelArea / resolutionArea * worldArea;
+      if (area >= MIN_BLOB_AREA && area <= MAX_BLOB_AREA) {
+        contours.push_back(*i);
+      }
+    }
   }
 
   void processContour(const vector<Point>& contour) {
     Mat blobImage = Mat::ones(height, width, CV_8UC1);
     for (vector<Point>::const_iterator i = contour.begin(); i != contour.end();
         i++) {
-      blobImage.at<uchar>(i->y, i->x) = 0;
+      blobImage.at < uchar > (i->y, i->x) = 0;
     }
 
-    floodFill(blobImage, Point(0,0), 0);
+    floodFill(blobImage, Point(0, 0), 0);
     blobImages.push_back(blobImage);
   }
 
   void render() {
-    glColor3f(0,0,1);
-    for(vector< vector<Point> >::iterator i = contours.begin(); i != contours.end(); i++) {
+    for (vector<vector<Point> >::iterator i = contours.begin();
+        i != contours.end(); i++) {
       drawContour(*i);
+      drawEstimatedOrientation(*i);
     }
 
-    glColor3f(1,0,0);
-    for(vector<Mat>::iterator i = blobImages.begin(); i != blobImages.end(); i++) {
+    glColor3f(1, 0, 0);
+    for (vector<Mat>::iterator i = blobImages.begin(); i != blobImages.end();
+        i++) {
       drawBlobImage(*i);
     }
   }
 
   void drawContour(const vector<Point>& contour) {
-    glBegin(GL_LINE_LOOP);
-    for(vector<Point>::const_iterator i = contour.begin(); i != contour.end(); i++) {
+    glColor3f(0, 0, 1);
+    glBegin (GL_LINE_LOOP);
+    for (vector<Point>::const_iterator i = contour.begin(); i != contour.end();
+        i++) {
       glVertex2i(i->x, i->y);
     }
+    glEnd();
+  }
+
+  void drawEstimatedOrientation(const vector<Point>& contour) {
+    glColor3f(0, 0, 1);
+    Vec4f v;
+    fitLine(contour, v, CV_DIST_L2, 0, 0.1, 0.1);
+    float vx = v[0];
+    float vy = v[1];
+    float x = v[2];
+    float y = v[3];
+    float leftY = -x * vy / vx + y;
+    float rightY = (width - x) * vy / vx + y;
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINES);
+    glVertex2i(0, leftY);
+    glVertex2i(width-1, rightY);
     glEnd();
   }
 
@@ -67,9 +113,12 @@ public:
     glEnd();
   }
 
-  void onKey(unsigned char key) {}
+  void onKey(unsigned char key) {
+  }
 
 private:
-  std::vector < std::vector<Point> > contours;
+  float worldArea;
+  float resolutionArea;
+  std::vector<std::vector<Point> > contours;
   vector<Mat> blobImages;
 };
