04e5068fe9d60dd0bcfb1e3dca617eda4f21bd68
Alex Berman <alex@nimations.com>
2017-02-11 10:29:59 +0100

    remove SimpleBlobDetector (replaced by lower-level solution with extraction of blob pixels)

--- blob_detection/src/BlobDetector.hpp
+++ blob_detection/src/BlobDetector.hpp
@@ -1,4 +1,3 @@
-#include <opencv2/features2d/features2d.hpp>
 #include <vector>
 
 using namespace std;
@@ -9,26 +8,9 @@ using namespace std;
 #include <GL/glut.h>
 #endif
 
-#define MIN_BLOB_DIAMETER_MM 200
-#define MAX_BLOB_DIAMETER_MM 1000
-
 class BlobDetector : public ProcessingMethod {
 public:
   BlobDetector(Tracker *tracker) : ProcessingMethod(tracker) {
-    float worldWidth = tracker->getWorldRange().xMax - tracker->getWorldRange().xMin;
-    float minWidth = MIN_BLOB_DIAMETER_MM * width / worldWidth;
-    float maxWidth = MAX_BLOB_DIAMETER_MM * width / worldWidth;
-
-    SimpleBlobDetector::Params params;
-    params.filterByColor = false;
-    params.filterByArea = true;
-    params.minArea = minWidth*minWidth;
-    params.maxArea = maxWidth*maxWidth;
-    params.filterByCircularity = false;
-    params.filterByInertia = false;
-    params.filterByConvexity = false;
-
-    detector = new SimpleBlobDetector(params);
   }
 
   void processDepthFrame(Mat& depthFrame) {
@@ -38,7 +20,6 @@ public:
     blobImages.clear();
     for(vector< vector<Point> >::iterator i = contours.begin(); i != contours.end(); i++)
       processContour(*i);
-    detector->detect(depthFrame, keyPoints);
   }
 
   void processContour(const vector<Point>& contour) {
@@ -53,11 +34,6 @@ public:
   }
 
   void render() {
-    glColor3f(0,1,0);
-    for(vector<KeyPoint>::iterator i = keyPoints.begin(); i != keyPoints.end(); i++) {
-      drawCircle(i->pt.x / width, i->pt.y / height, i->size / 2 / width);
-    }
-
     glColor3f(0,0,1);
     for(vector< vector<Point> >::iterator i = contours.begin(); i != contours.end(); i++) {
       drawContour(*i);
@@ -67,19 +43,6 @@ public:
     for(vector<Mat>::iterator i = blobImages.begin(); i != blobImages.end(); i++) {
       drawBlobImage(*i);
     }
-  }
-
-  void drawCircle(float centerX, float centerY, float radius) {
-    static const int RESOLUTION = 20;
-    glBegin(GL_LINE_LOOP);
-    float a, x, y;
-    for(int i=0; i<RESOLUTION; i++) {
-      a = 2 * M_PI * i/RESOLUTION;
-      x = centerX + cos(a) * radius;
-      y = centerY + sin(a) * radius;
-      glVertex2f(x, y);
-    }
-    glEnd();
   }
 
   void drawContour(const vector<Point>& contour) {
@@ -107,8 +70,6 @@ public:
   void onKey(unsigned char key) {}
 
 private:
-  SimpleBlobDetector *detector;
-  std::vector<KeyPoint> keyPoints;
   std::vector < std::vector<Point> > contours;
   vector<Mat> blobImages;
 };
