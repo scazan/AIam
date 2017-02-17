6efc25376759be5c6c8c9511f93553ee428cd5e2
Alex Berman <alex@nimations.com>
2017-02-17 12:41:10 +0100

    border_sound.py: websockets support made optional, now needs to be activated with --websockets

--- SCENE44/border_sound.py
+++ SCENE44/border_sound.py
@@ -2,11 +2,17 @@ from audio import *
 import time
 from math import sqrt, cos, pi
 import threading
+import argparse
 
-import tornado.web
-import tornado.ioloop
-import tornado.websocket
-from tornado.httpserver import HTTPServer
+parser = argparse.ArgumentParser()
+parser.add_argument("--websockets", action="store_true")
+args = parser.parse_args()
+
+if args.websockets:
+	import tornado.web
+	import tornado.ioloop
+	import tornado.websocket
+	from tornado.httpserver import HTTPServer
 
 s = create_audio_server().boot()
 s.amp = 0.1
@@ -32,77 +38,84 @@ max_distance_to_border = 5000
 websocket_lock = threading.Lock()
 
 def update_sound(relative_distance_to_border):
-        freq = min_freq + (max_freq - min_freq) * relative_distance_to_border
-        sine_left.setFreq(freq)
-        sine_right.setFreq(freq)
+	freq = min_freq + (max_freq - min_freq) * relative_distance_to_border
+	sine_left.setFreq(freq)
+	sine_right.setFreq(freq)
 
 update_sound(0)
 
 def distance(p0, p1):
-    return sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+	return sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
 def handle_center(path, values, types, src, user_data):
 	global area_radius
-        global s
+	global s
 	user_id, x, y, z = values
 	center_distance = distance([x,z], center)
-        if center_distance > area_radius:
-                distance_to_border = center_distance - area_radius
-                relative_distance_to_border = min(distance_to_border / max_distance_to_border, 1)
-        else:
-                relative_distance_to_border = (area_radius - center_distance) / area_radius
-        relative_distance_to_border = (-cos(-relative_distance_to_border*pi)+1)/2
-        print relative_distance_to_border
-        update_sound(relative_distance_to_border)
-        send_to_websocket_clients(str(relative_distance_to_border))
+	if center_distance > area_radius:
+		distance_to_border = center_distance - area_radius
+		relative_distance_to_border = min(distance_to_border / max_distance_to_border, 1)
+	else:
+		relative_distance_to_border = (area_radius - center_distance) / area_radius
+		relative_distance_to_border = (-cos(-relative_distance_to_border*pi)+1)/2
+		print relative_distance_to_border
+		update_sound(relative_distance_to_border)
+		if args.websockets:
+			send_to_websocket_clients(str(relative_distance_to_border))
 
 def send_to_websocket_clients(string):
-        with websocket_lock:
-                for client_handler in websocket_server.client_handlers:
-                        client_handler.write_message(string)
+	with websocket_lock:
+		for client_handler in websocket_server.client_handlers:
+			client_handler.write_message(string)
 
 osc_receiver = OscReceiver(OSC_PORT)
 osc_receiver.add_method("/center", "ifff", handle_center)
 osc_receiver.start()
 
-class ClientHandler(tornado.websocket.WebSocketHandler):
-    def __init__(self, server, request, **kwargs):
-        super(ClientHandler, self).__init__(server, request, **kwargs)
-        self._server = server
-        with websocket_lock:
-                server.client_handlers.add(self)
+if args.websockets:
+	class ClientHandler(tornado.websocket.WebSocketHandler):
+		def __init__(self, server, request, **kwargs):
+			super(ClientHandler, self).__init__(server, request, **kwargs)
+			self._server = server
+			with websocket_lock:
+				server.client_handlers.add(self)
 
-    def on_close(self):
-        with websocket_lock:
-                self._server.client_handlers.remove(self)
+		def on_close(self):
+			with websocket_lock:
+				self._server.client_handlers.remove(self)
 
-    def check_origin(self, origin):
-        return True
+		def check_origin(self, origin):
+			return True
 
-    def allow_draft76(self):
-        return True
+		def allow_draft76(self):
+			return True
 
-class WebsocketServer(tornado.web.Application):
-    def __init__(self, client_handler=ClientHandler, settings={}):
-        tornado.web.Application.__init__(
-            self,
-            [(WEBSOCKET_APPLICATION, client_handler, settings)],
-            debug=True)
-        self._loop = tornado.ioloop.IOLoop.instance()
-        self._listen(WEBSOCKET_PORT)
-        self.client_handlers = set()
+	class WebsocketServer(tornado.web.Application):
+		def __init__(self, client_handler=ClientHandler, settings={}):
+			tornado.web.Application.__init__(
+				self,
+				[(WEBSOCKET_APPLICATION, client_handler, settings)],
+				debug=True)
+			self._loop = tornado.ioloop.IOLoop.instance()
+			self._listen(WEBSOCKET_PORT)
+			self.client_handlers = set()
 
-    def _listen(self, port, address="", **kwargs):
-        self._server = HTTPServer(self, **kwargs)
-        self._server.listen(port, address)
+		def _listen(self, port, address="", **kwargs):
+			self._server = HTTPServer(self, **kwargs)
+			self._server.listen(port, address)
 
-    def start(self):
-        self._loop.start()
+		def start(self):
+			self._loop.start()
 
-    def stop(self):
-        self._loop.stop()
-        self._server.stop()
+		def stop(self):
+			self._loop.stop()
+			self._server.stop()
 
-websocket_server = WebsocketServer()
-s.start()
-websocket_server.start()
+	websocket_server = WebsocketServer()
+	s.start()
+	websocket_server.start()
+else:
+	s.start()
+	while True:
+		time.sleep(1)
+
