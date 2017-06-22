0293d6fc47278895d03e7f3ac0f703da08aec84c
Alex Berman <alex@nimations.com>
2017-06-22 07:13:35 +0200

    Enable UI to modify IO blending while stopped

--- dance-cognition/dimensionality_reduction/dimensionality_reduction_experiment.py
+++ dance-cognition/dimensionality_reduction/dimensionality_reduction_experiment.py
@@ -354,10 +354,7 @@ class DimensionalityReductionExperiment(Experiment):
                 self.input = self._follow.get_input()
 
             if self.args.enable_io_blending and self.input is not None and self.output is not None:
-                io_blend = numpy.array(self.output) * self._io_blending + \
-                           numpy.array(self.input) * (1-self._io_blending)
-                processed_io_blend = self._io_blending_entity.process_output(io_blend)
-                self.send_event_to_ui(Event(Event.IO_BLEND, processed_io_blend))
+                self._update_io_blend_and_broadcast_it_to_ui()
             
             if self.input is not None and self.args.incremental:
                 self.student.train([self.input])
@@ -391,7 +388,13 @@ class DimensionalityReductionExperiment(Experiment):
             self.entity.parameters_to_processed_pose(self.output, self._pose_for_feature_extraction)
             features = self.entity.extract_features(self._pose_for_feature_extraction)
             self.send_event_to_ui(Event(Event.FEATURES, features))
-    
+
+    def _update_io_blend_and_broadcast_it_to_ui(self):
+        io_blend = numpy.array(self.output) * self._io_blending + \
+                   numpy.array(self.input) * (1-self._io_blending)
+        processed_io_blend = self._io_blending_entity.process_output(io_blend)
+        self.send_event_to_ui(Event(Event.IO_BLEND, processed_io_blend))
+                
     def process_and_broadcast_output(self):
         if not (self._mode == modes.IMITATE and
                 self._imitate.showing_feature_matches()):
@@ -476,6 +479,7 @@ class DimensionalityReductionExperiment(Experiment):
 
     def _set_io_blending(self, event):
         self._io_blending = event.content
+        self._update_io_blend_and_broadcast_it_to_ui()
         
     def _plot_model(self):
         from plotting.model_plotter import ModelPlotter
