487a19a2391c4ec77d2007566b1e2998691efc22
Alex Berman <alex@nimations.com>
2017-07-10 15:49:27 +0200

    Enable input from PN in standalone application
    This change enables live input from PN in the standalone application. Additionally, the previous script only_ai.py is divided into improvising_avatars.py and single_learning_avatar.py (but input from PN is supported by both of them).

---
+++ movement_ai/application.py
@@ -0,0 +1,93 @@
+import time
+import numpy
+import random
+import collections
+
+from entities.hierarchical import Entity
+from fps_meter import FpsMeter
+
+class Avatar:
+    def __init__(self, index, entity, behavior):
+        self.index = index
+        self.entity = entity
+        self.behavior = behavior
+        
+class Application:
+    @staticmethod
+    def add_parser_arguments(parser):
+        parser.add_argument("--frame-rate", type=float, default=50.0)
+        parser.add_argument("--show-fps", action="store_true")
+        parser.add_argument("--output-receiver-host")
+        parser.add_argument("--output-receiver-port", type=int, default=10000)
+        parser.add_argument("--random-seed", type=int)
+        parser.add_argument("--memory-size", type=int, default=1000)
+        Entity.add_parser_arguments(parser)
+        
+    def __init__(self, student, avatars, args):
+        self._student = student
+        self._avatars = avatars
+        self._args = args
+
+        if args.random_seed is not None:
+            random.seed(args.random_seed)
+
+        if args.output_receiver_host:
+            from connectivity.simple_osc_sender import OscSender
+            self._output_sender = OscSender(port=args.output_receiver_port, host=args.output_receiver_host)
+        else:
+            self._output_sender = None
+            
+        if self._student.supports_incremental_learning():
+            self._training_data = collections.deque([], maxlen=args.memory_size)
+        
+    def run(self):
+        self._input = None
+        self._now = None
+        self._desired_frame_duration = 1.0 / self._args.frame_rate
+        self._frame_count = 0
+        if self._args.show_fps:
+            self._fps_meter = FpsMeter()
+        while True:
+            self._frame_start_time = time.time()
+            self._update_and_send_output()
+            self._wait_until_next_frame_is_timely()
+
+    def set_input(self, input_):
+        self._input = input_
+
+    def _update_and_send_output(self):
+        if self._input is not None and self._student.supports_incremental_learning():
+            self._student.train([self._input])
+            self._training_data.append(self._input)
+            self._student.probe(self._training_data)
+                
+        for avatar in self._avatars:
+            if self._input is not None and self._student.supports_incremental_learning():
+                avatar.behavior.set_normalized_observed_reductions(self._student.normalized_observed_reductions)
+            avatar.behavior.proceed(self._desired_frame_duration)
+            avatar.entity.update()
+            reduction = avatar.behavior.get_reduction(self._input)
+            if reduction is not None:
+                output = self._student.inverse_transform(numpy.array([reduction]))[0]
+                processed_output = avatar.entity.process_output(output)
+                if self._output_sender is not None:
+                    self._send_output(avatar.index, processed_output)
+
+        self.previous_frame_time = self._now
+        self._frame_count += 1
+        if self._args.show_fps:
+            self._fps_meter.update()
+
+    def _send_output(self, avatar_index, processed_output):
+        self._output_sender.send("/avatar_begin", avatar_index)
+        for index, worldpos in enumerate(processed_output):
+            self._output_sender.send(
+                "/world", self._frame_count, index,
+                worldpos[0], worldpos[1], worldpos[2])
+        self._output_sender.send("/avatar_end")
+
+    def _wait_until_next_frame_is_timely(self):
+        frame_duration = time.time() - self._frame_start_time
+        if frame_duration < self._desired_frame_duration:
+            time.sleep(self._desired_frame_duration - frame_duration)
+        
---
+++ movement_ai/applications/single_learning_avatar.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+STUDENT_MODEL_PATH = "profiles/dimensionality_reduction/valencia_pn_autoencoder.model"
+SKELETON_DEFINITION = "scenes/pn-01.22_skeleton.bvh"
+DIMENSIONALITY_REDUCTION_TYPE = "AutoEncoder"
+DIMENSIONALITY_REDUCTION_ARGS = "--num-hidden-nodes=0 --learning-rate=0.005"
+ENTITY_ARGS = "-r quaternion --friction --translate"
+
+NUM_REDUCED_DIMENSIONS = 7
+Z_UP = False
+FLOOR = True
+MAX_NOVELTY = 1.4
+
+from argparse import ArgumentParser
+import threading
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/..")
+from application import Application, Avatar
+from entities.hierarchical import Entity
+from bvh.bvh_reader import BvhReader
+from dimensionality_reduction.behaviors.improvise import ImproviseParameters, Improvise
+from dimensionality_reduction.factory import DimensionalityReductionFactory
+import tracking.pn.receiver
+
+parser = ArgumentParser()
+parser.add_argument("--pn-host", default="localhost")
+parser.add_argument("--pn-port", type=int, default=tracking.pn.receiver.SERVER_PORT_BVH)
+Application.add_parser_arguments(parser)
+ImproviseParameters().add_parser_arguments(parser)
+args = parser.parse_args()
+            
+bvh_reader = BvhReader(SKELETON_DEFINITION)
+bvh_reader.read()
+entity_args_strings = ENTITY_ARGS.split()
+entity_args = parser.parse_args(entity_args_strings)
+
+pose = bvh_reader.get_hierarchy().create_pose()
+entity = Entity(bvh_reader, pose, FLOOR, Z_UP, entity_args)
+
+num_input_dimensions = entity.get_value_length()
+student = DimensionalityReductionFactory.create(
+    DIMENSIONALITY_REDUCTION_TYPE, num_input_dimensions, NUM_REDUCED_DIMENSIONS, DIMENSIONALITY_REDUCTION_ARGS)
+student.load(STUDENT_MODEL_PATH)
+
+improvise_params = ImproviseParameters()
+preferred_location = None
+improvise = Improvise(
+    student,
+    student.num_reduced_dimensions,
+    improvise_params,
+    preferred_location,
+    MAX_NOVELTY)
+index = 0
+avatar = Avatar(index, entity, improvise)
+
+avatars = [avatar]
+
+application = Application(student, avatars, args)
+
+def receive_from_pn(pn_entity):
+    for frame in pn_receiver.get_frames():
+        input_from_pn = pn_entity.get_value_from_frame(frame)
+        application.set_input(input_from_pn)
+        
+pn_receiver = tracking.pn.receiver.PnReceiver()
+print "connecting to PN server..."
+pn_receiver.connect(args.pn_host, args.pn_port)
+print "ok"
+pn_pose = bvh_reader.get_hierarchy().create_pose()
+pn_entity = Entity(bvh_reader, pn_pose, FLOOR, Z_UP, entity_args)
+pn_receiver_thread = threading.Thread(target=lambda: receive_from_pn(pn_entity))
+pn_receiver_thread.daemon = True
+pn_receiver_thread.start()
+
+application.run()
