scazan/AIam

commit d973cb2c9678a2c2ff5b7d9e57c3c779f65a6c98

Optimization: Only create training data when needed
Before this change, dim_reduce.py -train always created training data and saved it to file, while all other training related commands loaded training data from file.

After this change, all training related commands in dim_reduce.py load the training data from file, if it exists, or creates and saves it files otherwize.

movement_ai/dimensionality_reduction/dimensionality_reduction_experiment.py
@@ -184,18 +184,17 @@ class DimensionalityReductionExperiment(Experiment):
             num_input_dimensions, self.args.num_components, self.args)
             
         if self.args.training_data_stats:
-            self._training_data = storage.load(self._training_data_path)
+            self._prepare_training_data()
             self._print_training_data_stats()
 
         if self.args.train:
             teacher = Teacher(self.training_entity, self.args.training_data_frame_rate)
-            self._training_data = teacher.create_training_data(self._training_duration())
+            self._prepare_training_data()
             self._train_model()
             print "saving %s..." % self._student_model_path
             self.student.save(self._student_model_path)
             print "ok"
             storage.save(self.training_entity.model, self._entity_model_path)
-            storage.save(self._training_data, self._training_data_path)
 
         elif self.args.analyze_components:
             self._load_model()
@@ -205,7 +204,7 @@ class DimensionalityReductionExperiment(Experiment):
                 parameter_info_getter=self.entity.parameter_info).analyze()
 
         elif self.args.analyze_accuracy:
-            self._training_data = storage.load(self._training_data_path)
+            self._prepare_training_data()
             self._load_model()
             self.student.analyze_accuracy(self._training_data)
 
@@ -215,14 +214,14 @@ class DimensionalityReductionExperiment(Experiment):
 
         elif self.args.train_feature_matcher:
             self._load_model()
-            self._training_data = storage.load(self._training_data_path)
+            self._prepare_training_data()
             self._train_feature_matcher()
 
         elif self.args.plot_model:
             self._plot_model()
 
         elif self.args.plot_pose_map_contents:
-            self._training_data = storage.load(self._training_data_path)
+            self._prepare_training_data()
             self._plot_pose_map_contents()
             
         else:
@@ -234,7 +233,7 @@ class DimensionalityReductionExperiment(Experiment):
                     self.min_training_loss = self.args.target_training_loss
                     self._training_loss = None
                 else:
-                    self._training_data = storage.load(self._training_data_path)
+                    self._prepare_training_data()
 
                 self._parameter_sets = {}
                 if self.args.enable_follow:
@@ -261,6 +260,13 @@ class DimensionalityReductionExperiment(Experiment):
                 self._memory = Memory()
                 
             self.run_backend_and_or_ui()
+
+    def _prepare_training_data(self):
+        if os.path.exists(self._training_data_path):
+            self._training_data = storage.load(self._training_data_path)
+        else:
+            self._training_data = teacher.create_training_data(self._training_duration())
+            storage.save(self._training_data, self._training_data_path)
 
     def _create_follow_behavior(self):
         return Follow(self.student, self.training_entity, self.bvh_reader)

commit 0ce186b922ce6164c5217b9defaab29b282844ae

Fix -train bug
This change fixes a bug introduced in d973cb2c9678a2c2ff5b7d9e57c3c779f65a6c98, which caused "dim_reduce.py -train" to yield an exception when the training data file did not exist.

movement_ai/dimensionality_reduction/dimensionality_reduction_experiment.py
@@ -188,7 +188,6 @@ class DimensionalityReductionExperiment(Experiment):
             self._print_training_data_stats()
 
         if self.args.train:
-            teacher = Teacher(self.training_entity, self.args.training_data_frame_rate)
             self._prepare_training_data()
             self._train_model()
             print "saving %s..." % self._student_model_path
@@ -265,6 +264,7 @@ class DimensionalityReductionExperiment(Experiment):
         if os.path.exists(self._training_data_path):
             self._training_data = storage.load(self._training_data_path)
         else:
+            teacher = Teacher(self.training_entity, self.args.training_data_frame_rate)
             self._training_data = teacher.create_training_data(self._training_duration())
             storage.save(self._training_data, self._training_data_path)
 