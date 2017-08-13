scazan/AIam

commit 1fb6f221a100988c6e46c679190ce8c2793d3290

Add support for training until convergence
This changes adds the autoencoder argument --target-loss-slope. When specified (e.g. with value 0.000001), training will stop when the recent mean downward slope of the loss reaches the specified value.

Additionally, --num-training-epochs is now optional.

movement_ai/dimensionality_reduction/autoencoder.py
@@ -6,13 +6,15 @@ import random
 from backports import tempfile
 from zipfile import ZipFile
 import os
+import collections
 
 class AutoEncoder(DimensionalityReduction):
     @staticmethod
     def add_parser_arguments(parser):
         parser.add_argument("--learning-rate", type=float, default=0.1)
         parser.add_argument("--num-hidden-nodes", type=int, default=3)
-        parser.add_argument("--num-training-epochs", type=int, default=1000)
+        parser.add_argument("--num-training-epochs", type=int)
+        parser.add_argument("--target-loss-slope", type=float)
         parser.add_argument("--tied-weights", action="store_true")
 
     def __init__(self, num_input_dimensions, num_reduced_dimensions, args):
@@ -37,15 +39,42 @@ class AutoEncoder(DimensionalityReduction):
         with self._graph.as_default():
             self._train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(self._cost)
 
-    def batch_train(self, training_data, num_training_epochs):
+    def batch_train(self,
+                    training_data,
+                    num_training_epochs=None,
+                    target_training_loss=None,
+                    target_loss_slope=None):
+        if target_loss_slope:
+            loss_slope_history = collections.deque(maxlen=10)
+            previous_loss = None
+        epoch = 0
         with self._graph.as_default():
             try:
-                for i in range(num_training_epochs):
-                    summary, _ = self._sess.run(
-                        [self._merged, self._train_step], feed_dict={self._input_layer: training_data})
-                    self._train_writer.add_summary(summary, i)
+                while True:
+                    loss, summary, _ = self._sess.run(
+                        [self._cost, self._merged, self._train_step],
+                        feed_dict={self._input_layer: training_data})
+                    self._train_writer.add_summary(summary, epoch)
+                    if num_training_epochs and epoch >= num_training_epochs:
+                        break
+                    if target_training_loss and loss <= target_training_loss:
+                        print "Stopping at epoch %d (loss %s <= %s)" % (
+                            epoch, loss, target_training_loss)
+                        break
+                    if target_loss_slope:
+                        if previous_loss is not None:
+                            loss_slope = previous_loss - loss
+                            loss_slope_history.append(loss_slope)
+                            if len(loss_slope_history) == 10:
+                                mean_loss_slope = sum(loss_slope_history) / len(loss_slope_history)
+                                if 0 <= mean_loss_slope <= target_loss_slope:
+                                    print "Stopping at epoch %d (loss slope: 0 <= %s <= %s)" % (
+                                        epoch, mean_loss_slope, target_loss_slope)
+                                    break
+                        previous_loss = loss
+                    epoch += 1
             except KeyboardInterrupt:
-                print "Training stopped at epoch %d" % i
+                print "Training stopped at epoch %d" % epoch
 
     def train(self, training_data, return_loss=False):
         with self._graph.as_default():

movement_ai/dimensionality_reduction/dimensionality_reduction_experiment.py
@@ -372,19 +372,14 @@ class DimensionalityReductionExperiment(Experiment):
             self.training_entity.probe(self._training_data)
             self._training_data = map(self.training_entity.adapt_value_to_model, self._training_data)
             print "ok"
-
+        
         print "training model..."
         if self.student.supports_incremental_learning():
-            if self.args.target_training_loss:
-                epoch = 0
-                while True:
-                    loss = self.student.train(self._training_data, return_loss=True)
-                    if loss <= self.args.target_training_loss:
-                        print "Stopping at epoch %d (%s <= %s)" % (epoch, loss, self.args.target_training_loss)
-                        break
-                    epoch += 1
-            else:
-                self.student.batch_train(self._training_data, self.args.num_training_epochs)
+            self.student.batch_train(
+                self._training_data,
+                self.args.num_training_epochs,
+                self.args.target_training_loss,
+                self.args.target_loss_slope)
         else:
             self.student.fit(self._training_data)
         print "ok"
@@ -413,8 +408,7 @@ class DimensionalityReductionExperiment(Experiment):
                 self.input = self._enqueue_and_deque_input(self._input_from_pn)
             else:
                 self.input = self._enqueue_and_deque_input(self._follow.get_input())
-                
-                
+
             if self.input is not None and self.student.supports_incremental_learning():
                 self._training_data.append(self.input)
                 if self.model_noise_to_add > 0:

commit f5f78311dcc5a35a2fa1819d83a120101cd901c1

Fix autoencoder --num-training-epochs bug
This change fixes a bug introduced in 1fb6f221a100988c6e46c679190ce8c2793d3290.

movement_ai/dimensionality_reduction/autoencoder.py
@@ -55,7 +55,7 @@ class AutoEncoder(DimensionalityReduction):
                         [self._cost, self._merged, self._train_step],
                         feed_dict={self._input_layer: training_data})
                     self._train_writer.add_summary(summary, epoch)
-                    if num_training_epochs and epoch >= num_training_epochs:
+                    if num_training_epochs is not None and epoch >= num_training_epochs:
                         break
                     if target_training_loss and loss <= target_training_loss:
                         print "Stopping at epoch %d (loss %s <= %s)" % (