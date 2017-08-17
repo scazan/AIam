efd5a394a17e4b13215da709eec7c691f79dd614
Alex Berman <alex@nimations.com>
2017-08-17 06:57:23 +0200

    pose_map_contents: Enable live input
    This change adds support for live input from PN and real-time learning in the pose_map_contents application.

--- movement_ai/applications/pose_map_contents.py
+++ movement_ai/applications/pose_map_contents.py
@@ -3,13 +3,14 @@
 STUDENT_MODEL_PATH = "profiles/dimensionality_reduction/valencia_pn_autoencoder.model"
 SKELETON_DEFINITION = "scenes/pn-01.22_skeleton.bvh"
 DIMENSIONALITY_REDUCTION_TYPE = "AutoEncoder"
-DIMENSIONALITY_REDUCTION_ARGS = "--num-hidden-nodes=0 --learning-rate=0.005 --tied-weights"
-ENTITY_ARGS = "-r quaternion --friction --translate"
+DIMENSIONALITY_REDUCTION_ARGS = "--num-hidden-nodes=0 --learning-rate=0.006 --tied-weights"
+ENTITY_ARGS = "-r quaternion --translate --translation-weight=0"
 
 NUM_REDUCED_DIMENSIONS = 7
 Z_UP = False
 FLOOR = True
-MAX_NOVELTY = 1.4
+
+FRAME_RATE = 50
 
 from argparse import ArgumentParser
 from OpenGL.GL import *
@@ -17,6 +18,8 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from PyQt4 import QtCore, QtGui, QtOpenGL
 import numpy
+import collections
+import threading
 
 import sys
 import os
@@ -28,8 +31,11 @@ import tracking.pn.receiver
 
 parser = ArgumentParser()
 Entity.add_parser_arguments(parser)
+parser.add_argument("--pn-host", default="localhost")
+parser.add_argument("--pn-port", type=int, default=tracking.pn.receiver.SERVER_PORT_BVH)
 parser.add_argument("--grid-resolution", type=int, default=5)
 parser.add_argument("--explore-beyond-observations", type=float, default=0.2)
+parser.add_argument("--memory-size", type=int, default=1000)
 args = parser.parse_args()
 
 bvh_reader = BvhReader(SKELETON_DEFINITION)
@@ -45,12 +51,20 @@ student = DimensionalityReductionFactory.create(
     DIMENSIONALITY_REDUCTION_TYPE, num_input_dimensions, NUM_REDUCED_DIMENSIONS, DIMENSIONALITY_REDUCTION_ARGS)
 student.load(STUDENT_MODEL_PATH)
 
+training_data = collections.deque([], maxlen=args.memory_size)
+input_from_pn = None
+
 class MainWindow(QtOpenGL.QGLWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self._explored_range = 1.0 + args.explore_beyond_observations
         self._explored_min = .5 - self._explored_range/2
         self._explored_max = .5 + self._explored_range/2
+        
+        timer = QtCore.QTimer(self)
+        timer.setInterval(1000. / FRAME_RATE)
+        QtCore.QObject.connect(timer, QtCore.SIGNAL('timeout()'), self.updateGL)
+        timer.start()
 
     def sizeHint(self):
         return QtCore.QSize(800, 800)
@@ -78,17 +92,35 @@ class MainWindow(QtOpenGL.QGLWidget):
         self.min_dimension = min(self.width, self.height)
 
     def paintGL(self):
+        global input_from_pn
+        if input_from_pn is None:
+            return
+        
+        student.train([input_from_pn])
+        training_data.append(input_from_pn)
+        student.probe(training_data)
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
         glTranslatef(self.margin, self.margin, 0)
-        self.render()
-
-    def render(self):
+        
         self.configure_2d_projection(0, self.width, 0, self.height)
+        
+        self._render_input(input_from_pn)
+        
         glColor3f(1,1,1)
         for grid_y in xrange(args.grid_resolution):
             for grid_x in xrange(args.grid_resolution):
                 self._render_cell(grid_x, grid_y)
+
+    def _render_input(self, input_):
+        vertices = entity.process_input(input_)
+        glColor3f(.5, .5, .5)
+        self._render_vertices_2d(
+            vertices,
+            x_offset=0.5,
+            y_offset=0.8,
+            scale=0.5)
     
     def configure_2d_projection(self, left, right, bottom, top):
         glMatrixMode(GL_PROJECTION)
@@ -110,7 +142,11 @@ class MainWindow(QtOpenGL.QGLWidget):
         
         glPushMatrix()
         glTranslatef(px, py, 0)
-        self._render_vertices_2d(vertices)
+        self._render_vertices_2d(
+            vertices,
+            x_offset=0.1,
+            y_offset=0.1,
+            scale=1.0/args.grid_resolution)
         glPopMatrix()
 
     def _get_normalized_reduction(self, grid_x, grid_y):
@@ -122,17 +158,16 @@ class MainWindow(QtOpenGL.QGLWidget):
                                       * self._explored_range + self._explored_min
         return normalized_reduction
         
-    def _render_vertices_2d(self, vertices):
+    def _render_vertices_2d(self, vertices, x_offset=0, y_offset=0, scale=1):
         screen_vertices = [
             self._world_to_screen(world_vertex)
             for world_vertex in vertices]
         screen_vertices = self._adjust_screen_vertices(
             screen_vertices,
-            x_offset=0.1,
-            y_offset=0,
-            scale=1.0/args.grid_resolution)
+            x_offset,
+            y_offset,
+            scale=scale)
         screen_edges = bvh_reader.vertices_to_edges(screen_vertices)
-        glColor3f(1, 1, 1)
         glLineWidth(2.0)
         for edge in screen_edges:
             self._render_edge_2d(edge.v1, edge.v2)
@@ -154,7 +189,23 @@ class MainWindow(QtOpenGL.QGLWidget):
         glVertex2f(v1[0] * self.width, v1[1] * self.height)
         glVertex2f(v2[0] * self.width, v2[1] * self.height)
         glEnd()    
+
         
+def receive_from_pn(pn_entity):
+    global input_from_pn
+    for frame in pn_receiver.get_frames():
+        input_from_pn = pn_entity.get_value_from_frame(frame)
+                
+pn_receiver = tracking.pn.receiver.PnReceiver()
+print "connecting to PN server..."
+pn_receiver.connect(args.pn_host, args.pn_port)
+print "ok"
+pn_pose = bvh_reader.get_hierarchy().create_pose()
+pn_entity = Entity(bvh_reader, pn_pose, FLOOR, Z_UP, entity_args)
+pn_receiver_thread = threading.Thread(target=lambda: receive_from_pn(pn_entity))
+pn_receiver_thread.daemon = True
+pn_receiver_thread.start()
+
 app = QtGui.QApplication(sys.argv)
 window = MainWindow()
 window.show()
