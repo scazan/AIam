scazan/AIam

commit 978fa6f6b27fdf0947266b82780bc26ed63d3040

ai_human_duet: Switch to zero-timeout QT timer
To check: Does this speed up things on osx?

movement_ai/application.py
@@ -45,9 +45,9 @@ class Application:
             
         self._training_data = collections.deque([], maxlen=args.memory_size)
         self._input = None
-        self._now = None
         self._desired_frame_duration = 1.0 / self._args.frame_rate
         self._frame_count = 0
+        self._previous_frame_time = None
         if self._args.show_fps:
             self._fps_meter = FpsMeter()
             
@@ -62,6 +62,12 @@ class Application:
 
     def set_student(self, student):
         self._student = student
+
+    def update_if_timely(self):
+        self._now = time.time()
+        if self._previous_frame_time is None or \
+           self._now - self._previous_frame_time >= self._desired_frame_duration:
+            self.update()
         
     def update(self):
         if self._input is not None and self._student.supports_incremental_learning():
@@ -87,7 +93,7 @@ class Application:
                 if self._output_sender is not None:
                     self._send_output(avatar, output)
 
-        self.previous_frame_time = self._now
+        self._previous_frame_time = self._now
         self._frame_count += 1
         if self._args.show_fps:
             self._fps_meter.update()

movement_ai/applications/ai_human_duet.py
@@ -414,8 +414,7 @@ class UiWindow(QtGui.QWidget):
         self._add_delay_shift_control()
         
         timer = QtCore.QTimer(self)
-        timer.setInterval(1000. / args.frame_rate)
-        QtCore.QObject.connect(timer, QtCore.SIGNAL('timeout()'), application.update)
+        QtCore.QObject.connect(timer, QtCore.SIGNAL('timeout()'), application.update_if_timely)
         timer.start()
 
     def _add_learning_rate_control(self):

commit 39b5ff49bbdf6639b3ddca8cce5eb379c2fbbfbc

Fix bug in application.py
This change fixes a bug introduced in 978fa6f6b27fdf0947266b82780bc26ed63d3040 which caused non-UI mode to break.

movement_ai/application.py
@@ -64,12 +64,12 @@ class Application:
         self._student = student
 
     def update_if_timely(self):
-        self._now = time.time()
         if self._previous_frame_time is None or \
-           self._now - self._previous_frame_time >= self._desired_frame_duration:
+           time.time() - self._previous_frame_time >= self._desired_frame_duration:
             self.update()
         
     def update(self):
+        now = time.time()
         if self._input is not None and self._student.supports_incremental_learning():
             self._student.train([self._input])
             self._training_data.append(self._input)
@@ -93,7 +93,7 @@ class Application:
                 if self._output_sender is not None:
                     self._send_output(avatar, output)
 
-        self._previous_frame_time = self._now
+        self._previous_frame_time = now
         self._frame_count += 1
         if self._args.show_fps:
             self._fps_meter.update()