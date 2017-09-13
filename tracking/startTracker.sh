7bcc56738e684e04e422fb19dca5e6906c8d88a8
Alex Berman <alex@nimations.com>
2017-09-13 16:58:34 +0200

    opencv tracking: Configure startTracker.sh for 4 cameras

--- tracking/startTracker.sh
+++ tracking/startTracker.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-./opencv/Tracker -zt 2800 -min-area 90000 -max-area 1000800 -device ./withBlobs.oni -camera-id 3
+
+MIN_AREA=220000
+MAX_AREA=1000800
+OSC_HOST=192.168.48.97
+TEMPORAL_SMOOTHING=1
+
+./opencv/Tracker -zt 2800 -min-area $MIN_AREA -max-area $MAX_AREA -ts $TEMPORAL_SMOOTHING -device 1d27/0601@1/64 -camera-id 0 -osc-host $OSC_HOST &
+./opencv/Tracker -zt 2800 -min-area $MIN_AREA -max-area $MAX_AREA -ts $TEMPORAL_SMOOTHING -device 1d27/0601@1/67 -camera-id 3 -osc-host $OSC_HOST &
+./opencv/Tracker -zt 2800 -min-area $MIN_AREA -max-area $MAX_AREA -ts $TEMPORAL_SMOOTHING -device 1d27/0601@1/70 -camera-id 1 -osc-host $OSC_HOST &
+./opencv/Tracker -zt 2800 -min-area $MIN_AREA -max-area $MAX_AREA -ts $TEMPORAL_SMOOTHING -device 1d27/0601@1/72 -camera-id 2 -osc-host $OSC_HOST &
